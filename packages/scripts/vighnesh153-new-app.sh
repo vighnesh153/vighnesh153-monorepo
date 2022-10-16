73a5e03ac88d6bcaa4d24767d595b79175726952
Vighnesh Raut <vighnesh.raut13@gmail.com>
2022-10-16 17:52:38 +0530

    cleanp

--- packages/scripts/vighnesh153-new-app.sh
+++ packages/scripts/vighnesh153-new-app.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-#ts-node-esm --project ./node_modules/@vighnesh153/scripts/tsconfig.json \
-#  ./node_modules/@vighnesh153/scripts/src/new-app/index.ts
 node ./node_modules/@vighnesh153/scripts/dist/new-app/index.js
