e35fe37a08b5d07fa14e7e8856b745ae3fc0a3ef
Vighnesh Raut <vighnesh.raut13@gmail.com>
2022-12-26 07:39:47 +0530

    feat(ui): add colors and IColor type

---
+++ packages/types/src/IColor.ts
@@ -0,0 +1,17 @@
+import { CSSProperties } from 'react';
+
+export interface IColor {
+  Shade0: CSSProperties['color'];
+  Shade1: CSSProperties['color'];
+  Shade2: CSSProperties['color'];
+  Shade3: CSSProperties['color'];
+  Shade4: CSSProperties['color'];
+  Shade5: CSSProperties['color'];
+  Shade6: CSSProperties['color'];
+  Shade7: CSSProperties['color'];
+  Shade8: CSSProperties['color'];
+  Shade9: CSSProperties['color'];
+  Shade10: CSSProperties['color'];
+  Shade11: CSSProperties['color'];
+  Shade12: CSSProperties['color'];
+}
