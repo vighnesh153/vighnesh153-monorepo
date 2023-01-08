3d1880f2eab667718c5604c92d3526fde6198a54
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-01-08 20:54:46 +0530

    feat: add get-in-touch link to the home page's introduction section

--- apps/vighnesh153.com/src/modules/home/components/sections/introduction-section.tsx
+++ apps/vighnesh153.com/src/modules/home/components/sections/introduction-section.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { alpha, Box, Typography, useTheme } from '@mui/material';
 import { homeModuleConstants } from '@modules/home/constants';
+import { BorderCornerAnimationLink, commonConstants, FocusDashedOutline } from '@modules/common';
 
 const introductionSection = homeModuleConstants.sections.introduction;
 
@@ -19,66 +20,73 @@ export function IntroductionSection() {
         flexDirection: 'column',
       }}
     >
-      <Typography component="h1">
-        <Typography
-          component="span"
-          variant="h6"
-          color={theme.palette.secondary.main}
-          sx={{
-            mb: '0.5rem',
-            display: 'block',
-          }}
-        >
-          {introductionSection.caption}
+      <FocusDashedOutline>
+        <Typography component="h1">
+          <Typography
+            component="span"
+            variant="h6"
+            color={theme.palette.secondary.main}
+            sx={{
+              mb: '0.5rem',
+              display: 'block',
+            }}
+          >
+            {introductionSection.caption}
+          </Typography>
+          <Typography
+            component="span"
+            variant="h2"
+            color={theme.palette.text.primary}
+            sx={{
+              mb: '0.5rem',
+              display: 'block',
+              fontWeight: 'bold !important',
+              typography: {
+                xs: 'h4',
+                sm: 'h3',
+                md: 'h2',
+              },
+            }}
+          >
+            {introductionSection.title}
+          </Typography>
+          <Typography
+            component="span"
+            variant="h2"
+            color={alpha(theme.palette.text.primary, 0.5)}
+            sx={{
+              mb: '1.5rem',
+              display: 'block',
+              fontWeight: 'bold !important',
+              typography: {
+                xs: 'h4',
+                sm: 'h3',
+                md: 'h2',
+              },
+            }}
+          >
+            {introductionSection.subtitle}
+          </Typography>
         </Typography>
         <Typography
-          component="span"
-          variant="h2"
-          color={theme.palette.text.primary}
+          component="p"
+          color={alpha(theme.palette.text.primary, 0.6)}
           sx={{
-            mb: '0.5rem',
             display: 'block',
-            fontWeight: 'bold !important',
             typography: {
-              xs: 'h4',
-              sm: 'h3',
-              md: 'h2',
+              xs: 'body1',
+              sm: 'h6',
             },
           }}
         >
-          {introductionSection.title}
+          {introductionSection.summary}
         </Typography>
-        <Typography
-          component="span"
-          variant="h2"
-          color={alpha(theme.palette.text.primary, 0.5)}
-          sx={{
-            mb: '1.5rem',
-            display: 'block',
-            fontWeight: 'bold !important',
-            typography: {
-              xs: 'h4',
-              sm: 'h3',
-              md: 'h2',
-            },
-          }}
-        >
-          {introductionSection.subtitle}
-        </Typography>
-      </Typography>
-      <Typography
-        component="p"
-        color={alpha(theme.palette.text.primary, 0.6)}
-        sx={{
-          display: 'block',
-          typography: {
-            xs: 'body1',
-            sm: 'h6',
-          },
-        }}
-      >
-        {introductionSection.summary}
-      </Typography>
+        <Box sx={{ mt: 5 }}>
+          <BorderCornerAnimationLink href={commonConstants.pageLinks.homePage.contactMeSection}>
+            Get in Touch
+          </BorderCornerAnimationLink>
+        </Box>
+      </FocusDashedOutline>
     </Box>
   );
 }
