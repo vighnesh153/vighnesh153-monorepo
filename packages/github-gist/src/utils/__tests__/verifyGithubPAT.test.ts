b4f22d9f24231f7387d545819bda5b1ee84c5571
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-02-09 19:58:24 +0530

    test: change axios.get() to axios()

--- packages/github-gist/src/utils/__tests__/verifyGithubPAT.test.ts
+++ packages/github-gist/src/utils/__tests__/verifyGithubPAT.test.ts
@@ -4,11 +4,17 @@ import { verifyGithubPAT } from '../verifyGithubPAT';
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
+function mockAxiosImplementation<T>(impl: () => Promise<T>) {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  mockedAxios.mockImplementation(impl);
+}
+
 describe('"verifyGithubPAT" function tests', () => {
   const GITHUB_PERSONAL_ACCESS_TOKEN = 'my-github-access-token';
 
   it('should throw error if API returns 4xx', async () => {
-    mockedAxios.get.mockImplementation(() => Promise.reject(new Error('Unauthorized')));
+    mockAxiosImplementation(() => Promise.reject(new Error('Unauthorized')));
 
     await expect(verifyGithubPAT(GITHUB_PERSONAL_ACCESS_TOKEN)).rejects.toThrowErrorMatchingInlineSnapshot(
       `"Token is invalid."`
@@ -16,7 +22,7 @@ describe('"verifyGithubPAT" function tests', () => {
   });
 
   it(`should throw error if token doesn't have "gist" oauth scope`, async () => {
-    mockedAxios.get.mockImplementation(() => {
+    mockAxiosImplementation(() => {
       const resolveValue = {
         headers: {
           'x-oauth-scopes': '',
@@ -31,7 +37,7 @@ describe('"verifyGithubPAT" function tests', () => {
   });
 
   it(`should not throw error if token is valid and has "gist" oauth scope`, async () => {
-    mockedAxios.get.mockImplementation(() => {
+    mockAxiosImplementation(() => {
       const resolveValue = {
         headers: {
           'x-oauth-scopes': 'gist',
