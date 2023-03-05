7d933b2169e055be0085c0f46c836b2ff6db6ef7
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-03-05 21:04:55 +0530

    refactor: ftp-server client data

--- packages/ftp-server/src/client/src/App.tsx
+++ packages/ftp-server/src/client/src/App.tsx
@@ -1,33 +1,16 @@
-import { useMemo } from 'react';
 import { Box, List } from '@mui/material';
-import { not } from '@vighnesh153/utils';
 import { DirectoryListItem, MainHeading, RegularFileListItem } from './components';
-import { isDirectory } from './utils';
+import { isDirectory, sortedDirectoryInformation } from './utils';
 import { PathBreadcrumbs } from './components/PathBreadcrumbs';
 
-const { directoryInformation } = window;
-
 export function App() {
-  const sortedFiles = useMemo(
-    () =>
-      directoryInformation.sort((file1, file2) => {
-        const isFile1Dir = isDirectory(file1.type);
-        const isFile2Dir = isDirectory(file2.type);
-
-        if (isFile1Dir && not(isFile2Dir)) return -1;
-        if (not(isFile1Dir) && isFile2Dir) return 1;
-        return file1.name.localeCompare(file2.name);
-      }),
-    []
-  );
-
   return (
     <Box sx={{ m: 4 }}>
       <MainHeading />
       <Box sx={{ height: 20 }} />
       <PathBreadcrumbs />
       <List sx={{ p: 2, mt: 3, border: '1px solid lightgray' }}>
-        {sortedFiles.map((file, index) =>
+        {sortedDirectoryInformation.map((file, index) =>
           isDirectory(file.type) ? (
             <DirectoryListItem key={index} file={file} />
           ) : (
--- packages/ftp-server/src/client/src/utils/index.ts
+++ packages/ftp-server/src/client/src/utils/index.ts
@@ -1,3 +1,27 @@
+import { not } from '@vighnesh153/utils';
 import { Vighnesh153File } from '../../../types';
 
 export const isDirectory = (fileType: Vighnesh153File['type']) => fileType === 'directory';
+
+// Mock information for development
+// const directoryInformation: Vighnesh153File[] = [
+//   { name: 'usr', type: 'directory' },
+//   { name: 'bin', type: 'directory' },
+//   { name: 'etc', type: 'directory' },
+//   { name: 'opt', type: 'directory' },
+//   { name: 'var', type: 'directory' },
+//   { name: '.zshrc', type: 'file' },
+//   { name: '.bashrc', type: 'file' },
+//   { name: '.gitignore', type: 'file' },
+// ]
+
+const { directoryInformation } = window;
+
+export const sortedDirectoryInformation = directoryInformation.sort((file1, file2) => {
+  const isFile1Dir = isDirectory(file1.type);
+  const isFile2Dir = isDirectory(file2.type);
+
+  if (isFile1Dir && not(isFile2Dir)) return -1;
+  if (not(isFile1Dir) && isFile2Dir) return 1;
+  return file1.name.localeCompare(file2.name);
+});
