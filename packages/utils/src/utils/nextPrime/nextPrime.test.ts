b0bf876d234c16da7e19ee511c964486aa10888d
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-03-17 16:19:19 +0530

    feat: add nextPrime function in @vighnesh153/utils

---
+++ packages/utils/src/utils/nextPrime/nextPrime.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, test } from 'vitest';
+import { nextPrime } from './nextPrime';
+
+describe('Math > Numbers > Primes > nextPrime tests', () => {
+  test.each([
+    [2, 3],
+    [3, 5],
+    [7.5, 11],
+    [15, 17],
+  ])(`nextPrime(%j) equals %j`, (current, expected) => {
+    expect(nextPrime(current)).toBe(expected);
+  });
+});
