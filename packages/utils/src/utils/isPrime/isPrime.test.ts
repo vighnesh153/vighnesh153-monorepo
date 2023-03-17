6f68eb8fa244337b4fc64a733872d260e9da5bd4
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-03-17 16:16:48 +0530

    feat: add isPrime function in @vighnesh153/utils

---
+++ packages/utils/src/utils/isPrime/isPrime.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, test } from 'vitest';
+import { isPrime } from './isPrime';
+
+describe('Math > Numbers > Primes > isPrime tests', () => {
+  test.each([
+    [1.5, false],
+    [-2, false],
+    [1, false],
+    [3, true],
+    [42, false],
+    [97, true],
+    [437, false],
+    [899, false],
+  ])('isPrime(%i)', (n, expected) => {
+    expect(isPrime(n)).toBe(expected);
+  });
+});
