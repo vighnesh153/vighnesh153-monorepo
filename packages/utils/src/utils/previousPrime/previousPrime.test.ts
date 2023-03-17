44f9c1247b87642c8b29fae666665fb52da5d0ed
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-03-17 16:25:10 +0530

    feat: add previousPrime function in @vighnesh153/utils

---
+++ packages/utils/src/utils/previousPrime/previousPrime.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, test } from 'vitest';
+import { previousPrime } from './previousPrime';
+
+describe('Math > Numbers > Primes > previousPrime tests', () => {
+  test.each([
+    [2, null],
+    [3, 2],
+    [7.5, 7],
+    [15, 13],
+  ])(`previousPrime(%j) equals %j`, (current, expected) => {
+    expect(previousPrime(current)).toBe(expected);
+  });
+});
