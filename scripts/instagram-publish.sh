f7b6921e685b1d64d97c0b5563539822ad3c80a4
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-03-25 11:34:18 +0530

    refactor: rename ui-automation to instagram-automation and add initial shell script

---
+++ scripts/instagram-publish.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+# Pull in all the secrets
+# Check the "secrets-template.sh"
+source "$(dirname "$0")/secrets.sh"
+
+# download random image
+echo "Starting download of a random image... ⏳"
+npm -w @vighnesh153/instagram-automation run openai:random-image
+echo "Downloading of random image complete ✅"
+
+# Pull in the instagram credentials for virtual vibrance account
+virtualVibranceCypressInstagramCredentials
+
+# Publish random image to instagram
+echo "Publishing random image to instagram account: virtual.vibrance... ⏳"
+npm -w @vighnesh153/instagram-automation run cypress:run -- \
+  --spec "cypress/e2e/instagram-ai-image.cy.ts" --browser electron --headed
+echo "Published random to instagram account: virtual.vibrance ✅"
---
+++ scripts/secrets-template.sh
@@ -0,0 +1,7 @@
+# How to use this script?
+# 1. Create a new script in the current directory called "secrets.sh"
+# 2. Give it execute permissions by running: "chmod +x ./secrets.sh"
+# 3. Paste the content of this file in "secrets.sh"
+# 4. Substitute the values of the secrets
+
+
