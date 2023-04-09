96f83ad03aa36c28a6cfa869743984def35eb4ff
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-04-09 22:10:54 +0530

    chore: update priorities

--- apps/vighnesh153.com/src/pages/api/auth/[...nextauth].ts
+++ apps/vighnesh153.com/src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,9 @@ import NextAuth, { AuthOptions } from 'next-auth';
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 
 import { nextAuthMongoDbClientPromise } from '@lib/mongodb-next-auth';
-import { nextAuthSignInCallback, NextAuthGoogleProvider } from '@lib/helpers/next-auth/server';
+import { NextAuthGoogleProvider, nextAuthSignInCallback } from '@lib/helpers/next-auth/server';
+
+// const useSecureCookies = Boolean(process.env.VERCEL_URL);
 
 export const authOptions: AuthOptions = {
   adapter: MongoDBAdapter(nextAuthMongoDbClientPromise),
