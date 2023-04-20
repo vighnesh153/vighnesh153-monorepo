df596d55935899d01739f6add0464f215df121eb
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-04-20 22:25:43 +0530

    feat: add cloudflare-tools and update aws lambdas

---
+++ cloudflare-tools/open-dictionary-worker/src/index.ts
@@ -0,0 +1,39 @@
+import { Request } from '@cloudflare/workers-types';
+
+const prefix = 'https://raw.githubusercontent.com/vighnesh153/open-dictionary/main/data';
+const suffix = '_.json';
+
+// export interface Env {}
+
+function buildResponse(data: unknown, status: number): Response {
+  return new Response(JSON.stringify(data), {
+    headers: { 'Content-Type': 'application/json' },
+    status,
+  });
+}
+
+export default {
+  async fetch(request: Request): Promise<Response> {
+    const url = new URL(request.url);
+    const word = url.searchParams.get('word');
+
+    if (word === null) {
+      return buildResponse({ message: `Please provide word using the "?word=<YOUR-WORD>" format` }, 400);
+    }
+
+    const definitionUrl = [prefix, ...word.split(''), suffix].join('/');
+    const definitionText = await fetch(definitionUrl).then((res) => res.text());
+
+    if (definitionText === '404: Not Found') {
+      return buildResponse(
+        {
+          message: `Word "${word}" not found.`,
+          searchUrl: definitionUrl,
+        },
+        404
+      );
+    }
+    const definition = JSON.parse(definitionText);
+    return buildResponse(definition, 200);
+  },
+};
