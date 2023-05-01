0146b85b9a191ee86fea8b8c767069a6c859de49
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-05-01 12:25:06 +0530

    build: hate vercel for this https://github.com/vercel/next.js/issues/42213. TODO: look at migrating away from NextJS

--- apps/vighnesh153.com/next.config.js
+++ apps/vighnesh153.com/next.config.js
@@ -3,9 +3,6 @@ import { withAxiom } from 'next-axiom';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   transpilePackages: ['@vighnesh153/types', '@vighnesh153/ui'],
 };
 
