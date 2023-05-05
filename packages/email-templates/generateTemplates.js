5c1181b257fa2c8d5670579d784b90f5f10dc35a
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-05-05 15:48:44 +0530

    feat: remove email-templates for now

--- packages/email-templates/generateTemplates.js
+++
@@ -1,9 +0,0 @@
-// import { render } from '@react-email/render';
-// import Email from './emails/vercel-invite-user';
-// import Email from './emails/vercel-invite-user';
-// import d from "./emails/vercel-invite-user"
-
-// fs.mkdir('', { recursive: true });
-
-// const emailHtml = render(Email());
-// console.log(emailHtml);
