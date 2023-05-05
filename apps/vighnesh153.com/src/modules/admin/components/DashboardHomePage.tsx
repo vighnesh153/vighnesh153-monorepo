732ca335c13f69002f8a8874926866559efc5b63
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-05-05 19:31:27 +0530

    feat: add intro for /admin/dashboard and /admin/permissions pages

--- apps/vighnesh153.com/src/modules/admin/components/DashboardHomePage.tsx
+++ apps/vighnesh153.com/src/modules/admin/components/DashboardHomePage.tsx
@@ -1,5 +1,17 @@
+import { Typography } from '@mui/material';
+
 import { AdminPageLayout } from './AdminPageLayout';
+import { adminModuleConstants } from '../constants';
 
 export function DashboardHomePage() {
-  return <AdminPageLayout>Dashboard</AdminPageLayout>;
+  return (
+    <AdminPageLayout pageTitle={adminModuleConstants.pageTitles.dashboard}>
+      <Typography component="h1" variant="h3">
+        Dashboard
+      </Typography>
+      <Typography sx={{ mt: 2 }}>
+        Welcome to the admin panel. This is the control center for all administrative actions on vighnesh153.com
+      </Typography>
+    </AdminPageLayout>
+  );
 }
--- apps/vighnesh153.com/src/modules/admin/components/PermissionsHomePage.tsx
+++ apps/vighnesh153.com/src/modules/admin/components/PermissionsHomePage.tsx
@@ -1,5 +1,14 @@
+import { Typography } from '@mui/material';
+
 import { AdminPageLayout } from './AdminPageLayout';
+import { adminModuleConstants } from '../constants';
 
 export function PermissionsHomePage() {
-  return <AdminPageLayout>Permissions</AdminPageLayout>;
+  return (
+    <AdminPageLayout pageTitle={adminModuleConstants.pageTitles.permissions}>
+      <Typography component="h1" variant="h3">
+        Permissions
+      </Typography>
+    </AdminPageLayout>
+  );
 }
