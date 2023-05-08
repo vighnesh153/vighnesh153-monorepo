2dbc5f10ebedf707d635ce88100eaf1b42b0da6c
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-05-08 08:35:16 +0530

    perf: optimized bundle size

--- apps/vighnesh153.com_next13/src/ui-modules/common/components/SocialLinks.tsx
+++ apps/vighnesh153.com_next13/src/ui-modules/common/components/SocialLinks.tsx
@@ -1,35 +1,44 @@
 'use client';
 
-import { Box, Tooltip, TooltipProps } from '@mui/material';
+import { lazy, Suspense } from 'react';
+import { Box, TooltipProps } from '@mui/material';
 import { commonModuleConstants } from '../constants';
 import { MuiNextLink } from './MuiNextLink';
+
+const Tooltip = lazy(() => import('@mui/material/Tooltip'));
 
 export interface SocialLinksProps {
   placement?: TooltipProps['placement'];
 }
 
 export function SocialLinks({ placement }: SocialLinksProps) {
+  const link = (profile: (typeof commonModuleConstants.profiles)[number]) => (
+    <MuiNextLink
+      href={profile.link}
+      aria-label={`Link to ${profile.title}`}
+      sx={{
+        display: 'inline-block',
+
+        fontSize: 0,
+        color: (theme) => theme.palette.text.primary,
+        '&:is(:hover, :focus)': {
+          color: (theme) => theme.palette.secondary.main,
+        },
+      }}
+    >
+      <profile.Icon size="1.25rem" />
+    </MuiNextLink>
+  );
+
   return (
     <>
       {commonModuleConstants.profiles.map((profile) => (
         <Box component="li" key={profile.identifier}>
-          <Tooltip title={profile.title} placement={placement}>
-            <MuiNextLink
-              href={profile.link}
-              aria-label={`Link to ${profile.title}`}
-              sx={{
-                display: 'inline-block',
-
-                fontSize: 0,
-                color: (theme) => theme.palette.text.primary,
-                '&:is(:hover, :focus)': {
-                  color: (theme) => theme.palette.secondary.main,
-                },
-              }}
-            >
-              <profile.Icon size="1.25rem" />
-            </MuiNextLink>
-          </Tooltip>
+          <Suspense fallback={link(profile)}>
+            <Tooltip title={profile.title} placement={placement}>
+              {link(profile)}
+            </Tooltip>
+          </Suspense>
         </Box>
       ))}
     </>
