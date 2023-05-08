cbfd06b78e094d6ada403a9aa4db3f5bff40f57a
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-05-08 10:45:04 +0530

    chore: remove playground app

--- apps/playground/.eslintrc.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: ['vighnesh153/next-ts.eslintrc.cjs'],
-  parserOptions: {
-    project: './tsconfig.json',
-  },
-};
--- apps/playground/test-configuration/frontend/globalTeardown.ts
+++
@@ -1,3 +0,0 @@
-export default async function globalTeardown() {
-  // no-op
-}
