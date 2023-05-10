f9df7a917c94488d8d96fea922176a7eef76beda
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-05-10 20:36:33 +0530

    feat: remove Slide lazy loading

--- apps/vighnesh153.com_next13/src/ui-modules/common/components/HideOnScroll.tsx
+++ apps/vighnesh153.com_next13/src/ui-modules/common/components/HideOnScroll.tsx
@@ -1,19 +1,14 @@
 'use client';
 
-import { lazy, Suspense } from 'react';
-import { useScrollTrigger } from '@mui/material';
+import { useScrollTrigger, Slide } from '@mui/material';
 import { not } from '@vighnesh153/utils';
-
-const Slide = lazy(() => import('@mui/material/Slide'));
 
 export function HideOnScroll(props: { children: React.ReactElement }) {
   const trigger = useScrollTrigger();
 
   return (
-    <Suspense fallback={props.children}>
-      <Slide appear={false} direction="down" in={not(trigger)}>
-        {props.children}
-      </Slide>
-    </Suspense>
+    <Slide appear={false} direction="down" in={not(trigger)}>
+      {props.children}
+    </Slide>
   );
 }
