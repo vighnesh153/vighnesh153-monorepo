8b5741c6b7baba6b13365ebe800e4e86c86a998b
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-05-11 17:41:18 +0530

    feat: add admin link in navbar

---
+++ apps/vighnesh153.com_next13/src/ui-modules/common/hooks/useIsAdmin.ts
@@ -0,0 +1,9 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
+import { adminEmails } from '../constants';
+
+export function useIsAdmin() {
+  const session = useSession();
+  return { isAdmin: adminEmails.includes(session.data?.user?.email ?? '') };
+}
