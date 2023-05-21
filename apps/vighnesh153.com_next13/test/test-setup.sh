3a3df22e0ff590d8ae8fa96b34bd839e08fcea03
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-05-21 15:20:29 +0530

    test: add test configuration for vercel and docker-available environments

--- apps/vighnesh153.com_next13/test/test-setup.sh
+++ apps/vighnesh153.com_next13/test/test-setup.sh
@@ -2,55 +2,62 @@
 
 # Automatically exit shell script on error
 # https://stackoverflow.com/questions/2870992/automatic-exit-from-bash-shell-script-on-error
-set -euxo pipefail
+set -exo pipefail
 
 # logs the docker version to the console
 docker -v
 
-###########################################
-#  Open docker only if it is not running  #
-###########################################
-echo ""
-echo "Attempting to start docker..."
-if (! docker stats --no-stream ); then
-  # Mac OS command
-  open /Applications/Docker.app
-  # Waiting for initialization
-  while (! docker stats --no-stream ); do
-    # Docker takes a few seconds to initialize
-    echo "Waiting for Docker to launch..."
-    sleep 5
-  done
-  echo "🐳 ✅ Docker is up and running"
+function openDockerDaemonIfNotRunning {
+  ###########################################
+  #  Open docker only if it is not running  #
+  ###########################################
+  echo ""
+  echo "Attempting to start docker..."
+  if (! docker stats --no-stream ); then
+    # Mac OS command
+    open /Applications/Docker.app
+    # Waiting for initialization
+    while (! docker stats --no-stream ); do
+      # Docker takes a few seconds to initialize
+      echo "Waiting for Docker to launch..."
+      sleep 5
+    done
+    echo "🐳 ✅ Docker is up and running"
+  else
+    echo "🐳 ✅ Docker is already running"
+  fi
+}
+
+function closeExistingDockerContainers {
+  ########################################
+  #  Closing all existing containers...  #
+  ########################################
+  echo ""
+  echo "🐳 Attempting to close all existing containers (if any)"
+  docker compose -f ./test/docker-compose.test.yml down
+  echo "🐳 ✅ Containers closed successfully"
+}
+
+function turnOnDockerContainers {
+  #####################################
+  #  Turning up docker containers...  #
+  #####################################
+  echo ""
+  echo "🐳 Turning up docker containers..."
+  docker compose -f ./test/docker-compose.test.yml up -d
+  echo "🐳 ✅ Containers are up and running"
+}
+
+if [[ -z "${VERCEL}" ]]; then
+  # Not inside a vercel environment
+  openDockerDaemonIfNotRunning
+  closeExistingDockerContainers
+  turnOnDockerContainers
+  export DATABASE_URL="postgresql://postgres:example@localhost:5432/myApp?schema=public"
+  npm run prisma:generate
+  npm run prisma:migrate:deploy
 else
-  echo "🐳 ✅ Docker is already running"
+  # Inside of a vercel environment
+  npm run prisma:generate:vercel
+  npm run prisma:migrate:deploy:vercel
 fi
-
-########################################
-#  Closing all existing containers...  #
-########################################
-echo ""
-echo "🐳 Attempting to close all existing containers (if any)"
-docker compose -f ./test/docker-compose.test.yml down
-echo "🐳 ✅ Containers closed successfully"
-
-#####################################
-#  Turning up docker containers...  #
-#####################################
-echo ""
-echo "🐳 Turning up docker containers..."
-docker compose -f ./test/docker-compose.test.yml up -d
-echo "🐳 ✅ Containers are up and running"
-
-# Export db url
-export DATABASE_URL="postgresql://postgres:example@localhost:5432/myApp?schema=public"
-
-############################
-#  Prisma generate client  #
-############################
-npx prisma generate
-
-#######################################
-#  Prisma Migrations push to test db  #
-#######################################
-npm run prisma:migrate:deploy
