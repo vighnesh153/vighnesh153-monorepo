5a47e2440194beb0ff0c7dcfe8fafbbdb3537bd0
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-06-02 21:52:46 +0530

    fix: missing adb crashes the build

--- packages/tv-remote-via-adb/src/app/api/devices/route.ts
+++ packages/tv-remote-via-adb/src/app/api/devices/route.ts
@@ -4,11 +4,16 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   console.log('Inside /api/devices');
 
-  const devices = execSync('adb devices')
-    .toString()
-    .split('\n')
-    .filter((line) => Boolean(line))
-    .slice(1)
-    .map((device) => device.split('\t')[0]);
-  return NextResponse.json({ devices });
+  try {
+    const devices = execSync('adb devices')
+      .toString()
+      .split('\n')
+      .filter((line) => Boolean(line))
+      .slice(1)
+      .map((device) => device.split('\t')[0]);
+    return NextResponse.json({ devices });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ message: 'Failed to fetch devices', error: e }, { status: 500 });
+  }
 }
