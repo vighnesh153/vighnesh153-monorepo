9d700132ad474047ae88407699f6c5dfd739d4c5
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-06-24 17:17:46 +0530

    style: fix astro eslint config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -7,18 +7,28 @@ const removeIgnoredEslintFiles = async (files) => {
   return filteredFiles.join(' ');
 };
 
+const eslintCommand = `eslint --max-warnings=0 --fix`;
+const prettierCommand = 'prettier --write';
+
 export default {
-  '*.mdx': ['eslint --max-warnings=0 --fix', 'prettier --write'],
+  '*.mdx': [eslintCommand, prettierCommand],
+  '*.md': [prettierCommand],
+  // TODO: uncomment the following line when it starts to work
+  // To test if this will work,
+  //     make some eslint errors in ./nodejs-tools/nodejs-apps/vighnesh153.com_astro/src/layouts/Layout.astro
+  //     and then run this command: ./node_modules/.bin/eslint --max-warnings=0 --fix ./nodejs-tools/nodejs-apps/vighnesh153.com_astro/src/layouts/Layout.astro
+  '*.astro': [
+    // eslintCommand,
+    prettierCommand,
+  ],
+  '*.js': [prettierCommand],
   '*.{ts,tsx}': async (files) => {
     const eslintFiles = await removeIgnoredEslintFiles(files);
     return [
-      // ...eslintFiles.map((file) => `eslint --max-warnings=0 --fix ${file}`),
-      `eslint --max-warnings=0 --fix ${eslintFiles}`,
+      `${eslintCommand} ${eslintFiles}`,
       // Doing the following "map" approach to avoid this error
       // "ENAMETOOLONG: name too long"
-      ...files.map((file) => `prettier --write ${file}`),
+      ...files.map((file) => `${prettierCommand} ${file}`),
     ];
   },
-  '*.{js,jsx}': ['prettier --write'],
-  // '*.md': ['prettier --write'],
 };
