074609d35926309ef14579919b35951e4cdd316c
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-06-25 17:55:09 +0530

    feat: add social links component, normalize.css and reset.css

---
+++ nodejs-tools/nodejs-apps/vighnesh153.com_astro/src/constants/socialProfiles.ts
@@ -0,0 +1,31 @@
+import GithubIcon from '../icons/GithubIcon.astro';
+import LinkedInIcon from '../icons/LinkedInIcon.astro';
+import StackoverflowIcon from '../icons/StackoverflowIcon.astro';
+import CodepenIcon from '../icons/CodepenIcon.astro';
+
+export const socialProfiles = [
+  {
+    identifier: 'github',
+    title: 'Github profile',
+    link: 'https://github.com/vighnesh153',
+    Icon: GithubIcon,
+  },
+  {
+    identifier: 'linkedIn',
+    title: 'LinkedIn profile',
+    link: 'https://www.linkedin.com/in/vighnesh153',
+    Icon: LinkedInIcon,
+  },
+  {
+    identifier: 'stackoverflow',
+    title: 'Stackoverflow profile',
+    link: 'https://stackoverflow.com/users/8822610/vighnesh153',
+    Icon: StackoverflowIcon,
+  },
+  {
+    identifier: 'codepen',
+    title: 'Codepen profile',
+    link: 'https://codepen.io/vighnesh153',
+    Icon: CodepenIcon,
+  },
+];
