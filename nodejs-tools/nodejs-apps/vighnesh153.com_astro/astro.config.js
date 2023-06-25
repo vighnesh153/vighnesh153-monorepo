59ae29f36d0b05b56a7cad4e42e2e5ab380684a0
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-06-25 21:03:30 +0530

    build: configure postcss, astro optimizations, etc

--- nodejs-tools/nodejs-apps/vighnesh153.com_astro/astro.config.js
+++ nodejs-tools/nodejs-apps/vighnesh153.com_astro/astro.config.js
@@ -1,4 +1,28 @@
 import { defineConfig } from 'astro/config';
 
+import prefetch from '@astrojs/prefetch';
+import sitemap from '@astrojs/sitemap';
+import rome from 'astro-rome';
+import compress from 'astro-compress';
+import critters from 'astro-critters';
+// import worker from 'astrojs-service-worker';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    // import.meta.env.MODE === 'production' ? worker() : null,
+    sitemap(),
+    critters({ logger: 1 }),
+    prefetch(),
+    rome({ logger: 1 }),
+    compress({ logger: 1 }),
+  ],
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  vite: {
+    build: {
+      sourcemap: true,
+    },
+  },
+});
