231f2733a65131296eade643d1315eb608669f99
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-06-30 11:19:12 +0530

    feat: add about-me section

--- nodejs-tools/nodejs-apps/vighnesh153.com_astro/src/constants/homePage.ts
+++ nodejs-tools/nodejs-apps/vighnesh153.com_astro/src/constants/homePage.ts
@@ -1,12 +1,48 @@
 const hashTags = {
   main: 'main',
+  aboutMe: 'about-me',
   contactMe: 'contact-me',
 };
 
+const companyUrls = {
+  google: 'https://about.google',
+  amazon: 'https://www.aboutamazon.com/about-us',
+  smarterCodes: 'https://smarter.codes',
+  tavisca: 'https://tavisca.com',
+};
+
+const internalLinks = {
+  main: `/#${hashTags.main}`,
+  aboutMe: `/#${hashTags.aboutMe}`,
+  contactMe: `/#${hashTags.contactMe}`,
+};
+
+const currentTechnologies = [
+  {
+    text: 'Go programming language',
+    link: 'https://go.dev',
+  },
+  {
+    text: 'Kotlin programming language',
+    link: 'https://kotlinlang.org',
+  },
+  {
+    text: 'Typescript',
+    link: 'https://www.typescriptlang.org',
+  },
+  {
+    text: 'Amazon Web Services (AWS)',
+    link: 'https://aws.amazon.com',
+  },
+  {
+    text: 'Astro.build',
+    link: 'https://astro.build',
+  },
+];
+
 export const homePage = {
   hashTags,
-  internalLinks: {
-    main: `/#${hashTags.main}`,
-    contactMe: `/#${hashTags.contactMe}`,
-  },
+  internalLinks,
+  companyUrls,
+  currentTechnologies,
 };
