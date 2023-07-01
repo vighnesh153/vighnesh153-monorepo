58450fd989f01f3dffe467ed3cf3a99bc73edec8
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-07-01 18:52:27 +0530

    feat: add footer section and split constants in different files

---
+++ nodejs-tools/nodejs-apps/vighnesh153.com_astro/src/constants/companyUrls.ts
@@ -0,0 +1,6 @@
+export const companyUrls = {
+  google: 'https://about.google',
+  amazon: 'https://www.aboutamazon.com/about-us',
+  smarterCodes: 'https://smarter.codes',
+  tavisca: 'https://tavisca.com',
+};
--- nodejs-tools/nodejs-apps/vighnesh153.com_astro/src/constants/index.ts
+++ nodejs-tools/nodejs-apps/vighnesh153.com_astro/src/constants/index.ts
@@ -1,4 +1,7 @@
+export * from './companyUrls';
+export * from './currentTechnologies';
 export * from './emails';
+export * from './externalLinks';
 export * from './homePage';
 export * from './socialProfiles';
 export * from './stage';
