f152225de5f1daa092d2a0b7aad93f15cd257301
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-07-02 16:46:39 +0530

    fix: remove stupid critters package and fix some warnings

--- nodejs-tools/nodejs-apps/vighnesh153.com_astro/astro.config.ts
+++ nodejs-tools/nodejs-apps/vighnesh153.com_astro/astro.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import prefetch from '@astrojs/prefetch';
 import sitemap from '@astrojs/sitemap';
-import rome from 'astro-rome';
 import compress from 'astro-compress';
-import critters from 'astro-critters';
 // import worker from 'astrojs-service-worker';
 
 // https://astro.build/config
@@ -13,9 +11,7 @@ export default defineConfig({
   integrations: [
     // import.meta.env.MODE === 'production' ? worker() : null,
     sitemap(),
-    critters({ logger: 1 }),
     prefetch(),
-    rome({ logger: 1 }),
     compress({ logger: 1 }),
   ],
   build: {
