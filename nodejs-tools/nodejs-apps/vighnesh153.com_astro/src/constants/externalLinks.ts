2859768681633f5c55e46df8426f5b0dd93504f7
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-07-07 16:03:10 +0530

    feat: add other-awesome-projects and refactor projects section

--- nodejs-tools/nodejs-apps/vighnesh153.com_astro/src/constants/externalLinks.ts
+++ nodejs-tools/nodejs-apps/vighnesh153.com_astro/src/constants/externalLinks.ts
@@ -1,5 +1,6 @@
 export const externalLinks = {
-  githubRepo: `https://github.com/vighnesh153/vighnesh153-monorepo`,
+  githubRepo:
+    'https://github.com/vighnesh153/vighnesh153-monorepo/tree/main/nodejs-tools/nodejs-apps/vighnesh153.com_astro',
   blog: 'https://blog.vighnesh153.com',
   resume: 'https://bit.ly/vighnesh153-resume',
 };
--- nodejs-tools/nodejs-apps/vighnesh153.com_astro/src/constants/internalLinks.ts
+++ nodejs-tools/nodejs-apps/vighnesh153.com_astro/src/constants/internalLinks.ts
@@ -11,5 +11,6 @@ export const internalLinks = {
   projects: {
     root: '/projects',
     featured: `/projects#${hashTags.projects.featuredProjects}`,
+    otherAwesomeProjects: `/projects#${hashTags.projects.otherAwesomeProjects}`,
   },
 };
