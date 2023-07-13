1768e00faf2f1fcf4a545a9429d6754043b4e3c6
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-07-13 16:35:00 +0530

    refactor: begin migration to tailwindcss

---
+++ nodejs-tools/nodejs-apps/vighnesh153.com_astro/tailwind.config.js
@@ -0,0 +1,57 @@
+import plugin from 'tailwindcss/plugin';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      primary: 'rgb(144, 116, 220)',
+      secondary: 'rgb(0, 0, 0)',
+      accent: 'rgb(102, 255, 219)',
+      background: 'rgb(10, 24, 46)',
+      text: 'rgb(251, 226, 253)',
+
+      backgroundDark: 'rgb(3, 12, 28)',
+      backgroundLight: 'rgb(18, 36, 67)',
+      backgroundBlend: 'rgb(19, 44, 63)',
+
+      text2: 'rgba(251, 226, 253, 70%)',
+      text3: 'rgba(251, 226, 253, 60%)',
+      text4: 'rgba(251, 226, 253, 30%)',
+    },
+    fontFamily: {
+      sans: ['Helvetica', 'Arial', 'sans-serif'],
+    },
+    extend: {
+      transitionProperty: {
+        width: 'width',
+      },
+    },
+  },
+  // prevent the following classes (defined in base.scss) from getting deleted from final bundle
+  safelist: [
+    // links
+    'regular-link',
+    'primary-btn-link',
+    'secondary-btn-link',
+    // buttons
+    'base-button',
+    'primary-button',
+    'secondary-button',
+  ],
+  plugins: [
+    plugin(({ addBase, theme }) => {
+      addBase({
+        html: {
+          color: theme('colors.text'),
+          ':is(a, button, input, select, textarea):focus-visible': {
+            outlineWidth: '3px',
+            outlineStyle: 'dashed',
+            outlineOffset: '0.5rem',
+            outlineColor: theme('colors.accent'),
+          },
+        },
+      });
+    }),
+  ],
+};
