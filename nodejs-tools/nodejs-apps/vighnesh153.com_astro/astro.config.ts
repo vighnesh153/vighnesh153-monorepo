71ebbb2b9d1f0e38bad398e269a9aefefd08f466
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-07-15 15:01:50 +0530

    perf: optimize css

--- nodejs-tools/nodejs-apps/vighnesh153.com_astro/astro.config.ts
+++ nodejs-tools/nodejs-apps/vighnesh153.com_astro/astro.config.ts
@@ -2,19 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import prefetch from '@astrojs/prefetch';
 import compress from 'astro-compress';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [
-    tailwind({
-      configFile: './tailwind.config.js',
-      applyBaseStyles: false,
-    }),
-    prefetch(),
-    compress({ logger: 1 }),
-  ],
+  integrations: [prefetch(), compress({ logger: 1 })],
   build: {
     inlineStylesheets: 'auto',
     format: 'file',
