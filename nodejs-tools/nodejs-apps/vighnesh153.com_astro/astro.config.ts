e4f7c460a652e5cf7800acff5265859a9e5551cd
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-07-25 14:06:28 +0530

    feat: integrate svelte in vighnesh153-astro

--- nodejs-tools/nodejs-apps/vighnesh153.com_astro/astro.config.ts
+++ nodejs-tools/nodejs-apps/vighnesh153.com_astro/astro.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
 import prefetch from '@astrojs/prefetch';
 import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [prefetch(), compress({ logger: 1 })],
+  integrations: [svelte(), prefetch(), compress({ logger: 1 })],
   build: {
     inlineStylesheets: 'auto',
     format: 'file',
