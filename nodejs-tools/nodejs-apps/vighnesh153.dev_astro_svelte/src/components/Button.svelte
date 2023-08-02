c059f3c69655cfdc816c610447227d66b0787658
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-08-02 20:38:42 +0530

    refactor: rename vighnesh153.com to vighnesh153.dev and convert *.astro to *.svelte

---
+++ nodejs-tools/nodejs-apps/vighnesh153.dev_astro_svelte/src/components/Button.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { classes } from '@/utils';
+  import type { HTMLButtonAttributes } from 'svelte/elements';
+
+  interface $$Props extends HTMLButtonAttributes {
+    variant?: 'primary' | 'secondary';
+  }
+  export let variant: $$Props['variant'] = 'secondary';
+
+  const { class: className, ...props } = $$props;
+</script>
+
+<button class={classes(className, `${variant}-button`)} {...props} on:click={() => console.log("lolol")}>
+  <slot />
+</button>
---
+++ nodejs-tools/nodejs-apps/vighnesh153.dev_astro_svelte/src/components/projects/spl/Root.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import Button from '@/components/Button.svelte';
+  import Textarea from '@/components/projects/spl/Textarea.svelte';
+  import CodeConsole from '@/components/projects/spl/CodeConsole.svelte';
+  import { classes } from '@/utils';
+
+  function runProgram() {
+    console.log('running program');
+  }
+</script>
+
+<div
+  class={classes(`
+    w-full mt-10 min-h-[30rem]
+    
+    grid grid-cols-1 place-items-center gap-4
+    lg:grid-cols-3 lg:place-items-stretch
+  `)}
+>
+  <div class="lg:col-start-3 grid place-items-center">
+    <Button variant="primary">SPL Main Menu</Button>
+  </div>
+  <Textarea
+    className={classes(`
+      w-full
+      lg:col-span-2 lg:row-span-2 lg:col-start-1 lg:row-start-1
+
+      min-h-[30rem]
+      bg-backgroundDark
+      z-[1]
+      resize-none
+      rounded-md
+    `)}
+  />
+  <CodeConsole
+    on:run-program={runProgram}
+    className={classes(`
+      w-full
+      min-h-[30rem]
+      bg-backgroundLight
+      rounded-md
+    `)}
+  />
+</div>
