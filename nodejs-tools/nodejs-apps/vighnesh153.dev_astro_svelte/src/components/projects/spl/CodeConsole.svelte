f9ba59f3b4c8ec607160b60ac23b7700c1a5bed1
Vighnesh Raut <vighnesh.raut13@gmail.com>
2023-08-02 20:38:42 +0530

    refactor: rename vighnesh153.com to vighnesh153.dev and convert *.astro to *.svelte

---
+++ nodejs-tools/nodejs-apps/vighnesh153.dev_astro_svelte/src/components/projects/spl/CodeConsole.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import { classes } from '@/utils';
+
+  const dispatch = createEventDispatcher<{ 'run-program': null }>();
+
+  export let className: string;
+</script>
+
+<div class={classes(className)}>
+  <div class="bg-secondary p-2 text-center relative">
+    <button
+      class={classes(`
+      absolute left-2 
+      px-2
+      
+      bg-accent text-secondary
+      font-bold
+      
+      cursor-pointer
+      rounded-md
+    `)}
+      on:click={() => {
+        console.log('triggerring stuff');
+        dispatch('run-program');
+      }}
+    >
+      Run
+    </button>
+    <span>Console</span>
+  </div>
+</div>
