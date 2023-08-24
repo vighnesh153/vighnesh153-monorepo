2a4ba0cffc99a5cc3e6d139d4adaca2f0bfd4397
vighnesh153 <vighnesh.raut13@gmail.com>
2023-08-24 12:54:04 +0530

    feat: create jvm-tools directory

---
+++ jvm-tools/jetpack/compose-query/src/test/kotlin/dev/vighnesh153/compose/query/LibraryTest.kt
@@ -0,0 +1,12 @@
+package dev.vighnesh153.compose.query
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class LibraryTest {
+    @Test
+    fun someLibraryMethodReturnsTrue() {
+        val classUnderTest = Library()
+        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'")
+    }
+}
