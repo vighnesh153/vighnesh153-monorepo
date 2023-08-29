543beb9458852cd19b3bf1b0ba78ffdfb707df2d
vighnesh153 <vighnesh.raut13@gmail.com>
2023-08-29 11:15:24 +0530

    feat: add color switcher in toolbar

--- nodejs-tools/nodejs-apps/vighnesh153-astro-svelte/src/components/projects/drawing-app/PopupButton.svelte
+++ nodejs-tools/nodejs-apps/vighnesh153-astro-svelte/src/components/projects/drawing-app/PopupButton.svelte
@@ -3,13 +3,30 @@
 
   export let buttonClasses: string = '';
   export let buttonStyles: string = '';
+
+  type PopupState = 'open' | 'closed';
+
+  let popupState: PopupState = 'closed';
+
+  function togglePopup(state?: PopupState) {
+    // prettier-ignore
+    popupState = state !== undefined 
+      ? state 
+      : (popupState === 'open' ? 'closed' : 'open');
+  }
 </script>
 
 <div class="relative">
-  <button style={buttonStyles} class={classes(`w-11 h-11 rounded-full border-2 border-secondary`, buttonClasses)}>
+  <button
+    style={buttonStyles}
+    class={classes(`w-11 h-11 rounded-full border-2 border-secondary`, buttonClasses)}
+    on:click={() => togglePopup()}
+  >
     <slot />
   </button>
-  <div class="absolute top-16">
-    <slot name="popup-content">Popup content</slot>
-  </div>
+  {#if $$slots['popup-content'] && popupState === 'open'}
+    <div class="absolute top-16 -translate-x-1/2 z-[1000000] w-fit p-6 bg-text shadow-2xl shadow-primary rounded-lg">
+      <slot name="popup-content" {togglePopup} />
+    </div>
+  {/if}
 </div>
