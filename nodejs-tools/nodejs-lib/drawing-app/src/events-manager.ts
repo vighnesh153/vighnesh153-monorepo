50d593489c2bc80259051b6fed6eb6a54f7814ce
vighnesh153 <vighnesh.raut13@gmail.com>
2023-09-03 13:14:45 +0530

    feat: update events manager api and add tests

--- nodejs-tools/nodejs-lib/drawing-app/src/events-manager.ts
+++ nodejs-tools/nodejs-lib/drawing-app/src/events-manager.ts
@@ -1,59 +1,57 @@
-import { Queue, not } from '@vighnesh153/utils';
+import { Queue, Stack, not } from '@vighnesh153/utils';
 import { type AppEvent } from './events';
 
 export type EventsManager = {
-  events: AppEvent[];
-  eventProcessingQueue: Queue<AppEvent>; // list of event ids to be processed
-  eventIndexPointer: number | null;
+  undoEventsStack: Stack<AppEvent>;
+  redoEventsStack: Stack<AppEvent>;
+  pendingQueue: Queue<AppEvent>; // events pending to be processed
 };
 
 function isCommitEvent(event?: AppEvent): boolean {
   return event?.type === 'commit';
 }
 
+export function buildEventsManager(): EventsManager {
+  return {
+    undoEventsStack: new Stack(),
+    redoEventsStack: new Stack(),
+    pendingQueue: new Queue(),
+  };
+}
+
 export function isUndoAvailable(eventsManager: EventsManager): boolean {
-  if (eventsManager.eventIndexPointer === null) {
-    return false;
-  }
-  return true;
+  return eventsManager.undoEventsStack.size > 0;
 }
 
 export function isRedoAvailable(eventsManager: EventsManager): boolean {
-  if (eventsManager.eventIndexPointer === null) {
-    return eventsManager.events.length > 0;
-  }
-
-  // there is at least 1 event after the pointer
-  return eventsManager.eventIndexPointer < eventsManager.events.length - 1;
+  return eventsManager.redoEventsStack.size > 0;
 }
 
 // returns true, if undo was successful, else, false
 export function undo(eventsManager: EventsManager): boolean {
   if (not(isUndoAvailable(eventsManager))) false;
 
-  // Move pointer to previous commit:
-  //
-  // [ {...}, {...}, {...}, {type=commit}, {...}, {...}, {type=commit}]
-  //                                                           ^
-  //
-  // will become
-  //
-  // [ {...}, {...}, {...}, {type=commit}, {...}, {...}, {type=commit} ]
-  //                              ^
-  let pointer = eventsManager.eventIndexPointer!;
-  let event = eventsManager.events[pointer];
-  do {
-    pointer -= 1;
-    event = eventsManager.events[pointer];
-  } while (pointer >= 0 && not(isCommitEvent(event)));
+  let isFirstCommitEvent = true;
 
-  if (pointer < 0) {
-    eventsManager.eventIndexPointer = null;
-    eventsManager.eventProcessingQueue = new Queue();
-  } else {
-    eventsManager.eventIndexPointer = pointer;
-    eventsManager.eventProcessingQueue = new Queue(...eventsManager.events.slice(0, pointer));
+  while (eventsManager.undoEventsStack.size > 0) {
+    const event = eventsManager.undoEventsStack.pop();
+
+    // non-first commit event
+    if (isCommitEvent(event) && not(isFirstCommitEvent)) {
+      eventsManager.undoEventsStack.push(event);
+      break;
+    }
+
+    // first commit event
+    if (isCommitEvent(event)) {
+      isFirstCommitEvent = false;
+    }
+
+    eventsManager.redoEventsStack.push(event);
   }
+
+  // add events to pending queue
+  eventsManager.pendingQueue = new Queue(...eventsManager.undoEventsStack.toArray());
 
   return true;
 }
@@ -62,36 +60,23 @@ export function undo(eventsManager: EventsManager): boolean {
 export function redo(eventsManager: EventsManager): boolean {
   if (not(isRedoAvailable(eventsManager))) false;
 
-  // Move pointer to next commit:
-  //
-  // [ {...}, {...}, {...}, {type=commit}, {...}, {...}, {type=commit}]
-  //                              ^
-  //
-  // will become
-  //
-  // [ {...}, {...}, {...}, {type=commit}, {...}, {...}, {type=commit} ]
-  //                                                           ^
-  //
-  let pointer = eventsManager.eventIndexPointer ?? -1;
-  let event = eventsManager.events[pointer];
-  do {
-    pointer += 1;
-    event = eventsManager.events[pointer];
-  } while (pointer < eventsManager.events.length - 1 && not(isCommitEvent(event)));
+  while (eventsManager.redoEventsStack.size > 0) {
+    const event = eventsManager.redoEventsStack.pop();
+    eventsManager.undoEventsStack.push(event);
 
-  eventsManager.events = eventsManager.events.slice(eventsManager.eventIndexPointer ?? 0, pointer);
-  eventsManager.eventIndexPointer = pointer;
+    if (isCommitEvent(event)) {
+      break;
+    }
+
+    eventsManager.pendingQueue.pushRight(event);
+  }
 
   return true;
 }
 
 export function publishEvents(eventsManager: EventsManager, events: AppEvent[]): void {
-  // remove all events from current index position (which will clear the redo stack)
-  eventsManager.events.splice(eventsManager.eventIndexPointer ?? 0, eventsManager.events.length, ...events);
+  eventsManager.undoEventsStack.push(...events);
+  eventsManager.redoEventsStack = new Stack();
 
-  // push all event ids to eventProcessingQueue
-  eventsManager.eventProcessingQueue.pushRight(...events);
-
-  // update the pointer to the last index
-  eventsManager.eventIndexPointer = eventsManager.events.length - 1;
+  eventsManager.pendingQueue.pushRight(...events);
 }
