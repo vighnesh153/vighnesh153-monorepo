9852249c8c3baa406552b81918051f52ffb5d73f
vighnesh153 <vighnesh.raut13@gmail.com>
2023-09-23 13:22:24 +0530

    feat: populate graphics-projects-page

---
+++ nodejs-tools/nodejs-apps/vighnesh153-astro-svelte/src/components/projects/graphics/GraphicsProjectsCollection.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { graphicsProjects } from '@vighnesh153/graphics-programming';
+
+  import { classes } from '@/utils';
+</script>
+
+<div class={classes('grid gap-10', 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3')}>
+  {#each graphicsProjects as graphicsProject (graphicsProject.title)}
+    <a
+      class={classes(
+        'group/card',
+
+        'min-h-[180px] w-full p-2',
+
+        'flex flex-col items-center',
+
+        'cursor-pointer rounded-2xl',
+
+        'bg-backgroundDark',
+        'hover:bg-backgroundLight focus:bg-backgroundLight',
+
+        'shadow-md',
+        'hover:shadow-xl focus:shadow-xl',
+
+        'shadow-text4',
+        'hover:shadow-text4 focus:shadow-text4'
+      )}
+      href="/"
+    >
+      <img
+        class={classes('block w-10/12 h-4/5 object-contain', 'border rounded-xl border-text4')}
+        alt={graphicsProject.title}
+        src={graphicsProject.imageLink}
+      />
+      <p class={classes('mt-1 text-xl text-text2', 'group-hover/card:text-text group-focus/card:text-text')}>
+        {graphicsProject.title}
+      </p>
+    </a>
+  {/each}
+</div>
--- nodejs-tools/nodejs-lib/graphics-programming/src/collection.ts
+++ nodejs-tools/nodejs-lib/graphics-programming/src/collection.ts
@@ -1,76 +1,91 @@
 interface GraphicsProject {
   imageLink: string;
   title: string;
+  id: string;
 }
 
 const barnsleysFern: GraphicsProject = {
   imageLink: 'https://i.imgur.com/8F9Y1DQ.png',
   title: `Barnsley's Fern`,
+  id: 'barnsleys-fern',
 };
 
 const brickBreakerGame: GraphicsProject = {
   imageLink: 'https://i.imgur.com/C0N3twn.png',
   title: 'Brick Breaker Game',
+  id: 'brick-breaker-game',
 };
 
 const connectingParticles: GraphicsProject = {
   imageLink: 'https://i.imgur.com/ZKYXBhX.png',
   title: 'Connecting Particles',
+  id: 'connecting-particles',
 };
 
 const flappyBlockGame: GraphicsProject = {
   imageLink: 'https://i.imgur.com/hKgLkKy.png',
   title: 'Flappy Block Game',
+  id: 'flappy-block-game',
 };
 
 const gridPathFinderGame: GraphicsProject = {
   imageLink: 'https://i.imgur.com/j0JsNwY.png',
   title: 'Grid Path Finder',
+  id: 'grid-path-finder',
 };
 
 const pongGame: GraphicsProject = {
   imageLink: 'https://i.imgur.com/9c0kEFU.png',
   title: 'Pong Game',
+  id: 'pong-game',
 };
 
 const pseudoHilbertCurve: GraphicsProject = {
   imageLink: 'https://i.imgur.com/Lcyw7DQ.png',
   title: 'Pseudo Hilbert Curve',
+  id: 'pseudo-hilbert-curve',
 };
 
 const serpinskisTriangle: GraphicsProject = {
   imageLink: 'https://i.imgur.com/J2zUHcl.png',
   title: `Sierpinski's Triangle`,
+  id: 'sierpinskis-triangle',
 };
 
 const snakeGame: GraphicsProject = {
   imageLink: 'https://i.imgur.com/spwnsPX.png',
   title: 'Snake Game',
+  id: 'snake-game',
 };
 
 const sortingVisualizer: GraphicsProject = {
   imageLink: 'https://i.imgur.com/YHKu6TN.png',
   title: 'Sorting Visualizer',
+  id: 'sorting-visualizer',
 };
 
 const symmetricBinaryTree: GraphicsProject = {
   imageLink: 'https://i.imgur.com/tzA2Pyf.png',
   title: 'Symmetric Binary Tree',
+  id: 'symmetric-binary-tree',
 };
 
 const towerOfHanoi: GraphicsProject = {
   imageLink: 'https://i.imgur.com/kIsetz4.png',
   title: 'Tower of Hanoi',
+  id: 'tower-of-hanoi',
 };
 
 const treePathFinder: GraphicsProject = {
   imageLink: 'https://i.imgur.com/879Zf9b.png',
   title: 'Tree Path Finder',
+  id: 'tree-path-finder',
 };
 
 const twinklingStars: GraphicsProject = {
   imageLink: 'https://i.imgur.com/Zhk91iE.png',
   title: 'Twinkling Stars',
+  id: 'twinkling-stars',
 };
 
 export const graphicsProjects: GraphicsProject[] = [
