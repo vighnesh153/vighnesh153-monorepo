bef891d9b4521535c09efb82d3cf81169a31c191
vighnesh153 <vighnesh.raut13@gmail.com>
2023-09-25 06:39:20 +0530

    fix: remove trailing '.html' extension when comparing urls

--- nodejs-tools/nodejs-apps/vighnesh153-astro-svelte/src/utils/verifyGraphicsProjectPath.ts
+++ nodejs-tools/nodejs-apps/vighnesh153-astro-svelte/src/utils/verifyGraphicsProjectPath.ts
@@ -3,10 +3,14 @@ import { type GraphicsProject } from '@vighnesh153/graphics-programming';
 import { internalLinks } from '@/constants';
 
 export function verifyGraphicsProjectPath(project: GraphicsProject, url: string): void {
-  const actualPathName = new URL(url).pathname;
+  // remove trailing ".html"
+  const actualPathName = new URL(url).pathname.replace(/\.html$/, '');
   const expectedPathName = internalLinks.projects.graphicsProjects.buildProjectLinkFromId(project.id);
 
   if (actualPathName !== expectedPathName) {
-    throw new Error(`Path doesn't match for graphics project with title: ${project.title}`);
+    throw new Error(
+      `Path doesn't match for graphics project with title: ${project.title}. ` +
+        `Expected "${expectedPathName}", found "${actualPathName}"`
+    );
   }
 }
