aaf4236e30427e4ad904fad7b2467000cb6d036a
vighnesh153 <vighnesh.raut13@gmail.com>
2023-11-21 23:04:21 +0530

    refactor: improve the size finding logic

--- nodejs-tools/nodejs-lib/graphics-programming/src/pseudo-hilbert-curve/Game.ts
+++ nodejs-tools/nodejs-lib/graphics-programming/src/pseudo-hilbert-curve/Game.ts
@@ -11,11 +11,21 @@ export class PseudoHilbertCurveGame {
   }
 
   *start() {
-    const p1 = new Point(100, 100);
-    const p2 = new Point(100, 200);
-    const p3 = new Point(200, 200);
-    const p4 = new Point(200, 100);
-    const curves = generateCurve(p1, p2, p3, p4, 6);
+    const { width, height } = this.#canvasWrapper;
+    const level = 5;
+    const size = Math.round((height * 5) / 11);
+
+    const x1 = width / 2 - size / 2;
+    const x2 = x1 + size;
+    const y1 = height / 2 - size / 2;
+    const y2 = y1 + size;
+
+    const p1 = new Point(x1, y1);
+    const p2 = new Point(x1, y2);
+    const p3 = new Point(x2, y2);
+    const p4 = new Point(x2, y1);
+
+    const curves = generateCurve(p1, p2, p3, p4, level);
     this.drawLines(curves);
 
     yield;
