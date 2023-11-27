14d0cd8a52fbfea9b5f3c1434978c7c4d28f9f89
vighnesh153 <vighnesh.raut13@gmail.com>
2023-11-27 09:10:38 +0530

    refactor: bubble sort algorithm

--- nodejs-tools/nodejs-lib/graphics-programming/src/sorting-visualizer/SortingAlgorithmImpls.ts
+++ nodejs-tools/nodejs-lib/graphics-programming/src/sorting-visualizer/SortingAlgorithmImpls.ts
@@ -3,18 +3,42 @@ import { SortingAlgorithm } from './SortingAlgorithm';
 export class BubbleSortSortingAlgorithm extends SortingAlgorithm {
   *sort() {
     const { array } = this;
-    const size = this.array.length;
-    for (let i = 0; i < size; i++) {
-      for (let j = 0; j < size - i; j++) {
-        if (array[j] > array[j + 1]) {
-          const temp = array[j];
-          array[j] = array[j + 1];
-          array[j + 1] = temp;
+    const size = array.length;
 
-          this.modifiedIndices = [j, j + 1];
+    for (let i = 0; i < size; i++) {
+      // (size - 1) because we access the j + 1 index
+      // -i is just an optimization because the last i elements are already at correct positions
+      for (let j = 0; j < size - 1 - i; j++) {
+        if (array[j] > array[j + 1]) {
+          this.swap(j, j + 1);
+          this.updateModifiedIndices(j, j + 1);
           yield;
         }
       }
+    }
+    this.updateModifiedIndices(-1, -1);
+    yield;
+  }
+
+  private swap(i: number, j: number): void {
+    const { array } = this;
+    const size = array.length;
+
+    if (i >= size || j >= size) {
+      throw new Error(`Index out of bounds: i=${i}, j=${j}, size=${size}`);
+    }
+
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  private updateModifiedIndices(i: number, j: number): void {
+    if (this.modifiedIndices.length < 2) {
+      this.modifiedIndices = [i, j];
+    } else {
+      this.modifiedIndices[0] = i;
+      this.modifiedIndices[1] = j;
     }
   }
 }
