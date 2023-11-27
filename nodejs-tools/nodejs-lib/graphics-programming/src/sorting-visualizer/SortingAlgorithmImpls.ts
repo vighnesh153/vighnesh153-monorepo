e96cd06f5d147d508ef0467d30db7f7413ce1103
vighnesh153 <vighnesh.raut13@gmail.com>
2023-11-27 11:03:43 +0530

    feat: implement merge sort algorithm

--- nodejs-tools/nodejs-lib/graphics-programming/src/sorting-visualizer/SortingAlgorithmImpls.ts
+++ nodejs-tools/nodejs-lib/graphics-programming/src/sorting-visualizer/SortingAlgorithmImpls.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { SortingAlgorithm } from './SortingAlgorithm';
 
 export class BubbleSortSortingAlgorithm extends SortingAlgorithm {
@@ -44,8 +45,63 @@ export class BubbleSortSortingAlgorithm extends SortingAlgorithm {
 }
 
 export class MergeSortSortingAlgorithm extends SortingAlgorithm {
+  private originalArray: number[] = [];
+
   *sort() {
+    const { array } = this;
+    this.originalArray = [...array];
+    console.log('Starting');
+    const sortedArray = this.mergeSort(array, 0, array.length - 1);
+    this.array = sortedArray;
+    console.log('Ending');
     yield;
+  }
+
+  private mergeSort(array: number[], startIndex: number, endIndex: number): number[] {
+    if (startIndex === endIndex) return [array[startIndex]];
+
+    const midIndex = Math.floor((startIndex + endIndex) / 2);
+    const left = this.mergeSort(array, startIndex, midIndex);
+    const right = this.mergeSort(array, midIndex + 1, endIndex);
+
+    return this.merge(left, right);
+  }
+
+  private merge(array1: number[], array2: number[]): number[] {
+    let index1 = 0;
+    let index2 = 0;
+
+    const size1 = array1.length;
+    const size2 = array2.length;
+
+    let index = 0;
+    const mergedArray: number[] = Array.from({ length: size1 + size2 });
+    function addToMergedArray(entry: number): void {
+      mergedArray[index++] = entry;
+    }
+
+    while (index1 < size1 && index2 < size2) {
+      const element1 = array1[index1];
+      const element2 = array2[index2];
+
+      if (element1 < element2) {
+        addToMergedArray(element1);
+        index1++;
+      } else {
+        addToMergedArray(element2);
+        index2++;
+      }
+    }
+
+    while (index1 < size1) {
+      addToMergedArray(array1[index1++]);
+    }
+
+    while (index2 < size2) {
+      addToMergedArray(array2[index2++]);
+    }
+
+    return mergedArray;
   }
 }
 
