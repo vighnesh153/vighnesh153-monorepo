b61b614e04244d55468618479f565903af87781d
vighnesh153 <vighnesh.raut13@gmail.com>
2023-12-10 19:40:27 +0530

    feat: add twinkling-stars illustration

---
+++ nodejs-tools/nodejs-lib/graphics-programming/src/twinkling-stars/Game.ts
@@ -0,0 +1,63 @@
+import { CanvasWrapper } from '@/canvas-wrapper';
+import { getCanvasBgColor } from '@/getCanvasBgColor';
+import { Star } from './star';
+import { repeat } from '@vighnesh153/utils';
+
+interface GameOptions {
+  bgColor?: string;
+}
+
+export class TwinklingStarsGame {
+  readonly #canvasWrapper: CanvasWrapper;
+
+  readonly #bgColor: string;
+  readonly #stars: Star[] = [];
+
+  #isRunning = false;
+
+  constructor(canvasWrapper: CanvasWrapper, options: GameOptions = {}) {
+    this.#canvasWrapper = canvasWrapper;
+    this.#bgColor = options.bgColor ?? getCanvasBgColor(canvasWrapper);
+
+    repeat(200, () => {
+      this.#stars.push(new Star(canvasWrapper));
+    });
+  }
+
+  *start() {
+    this.#isRunning = true;
+    while (this.#isRunning) {
+      this.draw();
+      this.updateStars();
+      yield;
+    }
+  }
+
+  stop() {
+    this.#isRunning = false;
+  }
+
+  draw() {
+    this.clear();
+    this.drawStars();
+  }
+
+  update() {
+    this.updateStars();
+  }
+
+  private drawStars() {
+    this.#stars.forEach((star) => star.draw());
+  }
+
+  private updateStars() {
+    this.#stars.forEach((star) => star.update());
+  }
+
+  clear() {
+    const rect = this.#canvasWrapper.getBoundingClientRect();
+    const canvasWidth = rect.width;
+    const canvasHeight = rect.height;
+    this.#canvasWrapper.drawFilledRect(0, 0, canvasWidth, canvasHeight, this.#bgColor);
+  }
+}
---
+++ nodejs-tools/nodejs-lib/graphics-programming/src/twinkling-stars/star.ts
@@ -0,0 +1,101 @@
+import { randomInteger } from '@vighnesh153/utils';
+import { CanvasWrapper } from '@/canvas-wrapper';
+
+interface StarColor {
+  r: number;
+  g: number;
+  b: number;
+}
+
+interface StarOptions {
+  color?: StarColor;
+  velocityMultiplier?: number;
+}
+
+interface Position {
+  x: number;
+  y: number;
+}
+
+interface Velocity {
+  dx: number;
+  dy: number;
+}
+
+export class Star {
+  readonly #canvasWrapper: CanvasWrapper;
+
+  readonly #color: StarColor;
+  readonly #radius = randomInteger(2, 5);
+  readonly #position: Position;
+  readonly #velocity: Velocity;
+
+  #opacity: number;
+  #dOpacity: number;
+
+  constructor(canvasWrapper: CanvasWrapper, options: StarOptions = {}) {
+    this.#canvasWrapper = canvasWrapper;
+    const velocityMultiplier = options.velocityMultiplier ?? 0.25;
+
+    this.#color = options.color ?? {
+      r: 0,
+      g: 0,
+      b: 0,
+    };
+
+    this.#position = {
+      x: this.coerceX(randomInteger(1, 100000)),
+      y: this.coerceY(randomInteger(1, 100000)),
+    };
+
+    this.#velocity = {
+      dx: randomInteger(-1, 1) * velocityMultiplier,
+      dy: randomInteger(-1, 1) * velocityMultiplier,
+    };
+
+    this.#opacity = 1;
+    this.#dOpacity = Math.random() / 50;
+  }
+
+  draw(): void {
+    const radius = this.#radius;
+    const opacity = this.#opacity;
+    const { x, y } = this.#position;
+    const { r, g, b } = this.#color;
+
+    this.#canvasWrapper.drawFilledCircle(x, y, radius, `rgba(${r}, ${g}, ${b}, ${opacity})`);
+  }
+
+  update() {
+    this.updatePosition();
+    this.updateOpacity();
+  }
+
+  private updatePosition() {
+    const pos = this.#position;
+    const { dx, dy } = this.#velocity;
+    pos.x = this.coerceX(pos.x + dx);
+    pos.y = this.coerceY(pos.y + dy);
+  }
+
+  private updateOpacity() {
+    this.#opacity += this.#dOpacity;
+
+    if (this.#opacity >= 1) {
+      this.#dOpacity = -Math.abs(this.#dOpacity);
+    }
+    if (this.#opacity <= 0) {
+      this.#dOpacity = Math.abs(this.#dOpacity);
+    }
+  }
+
+  private coerceX(x: number): number {
+    const w = this.#canvasWrapper.width;
+    return (w + x) % w;
+  }
+
+  private coerceY(y: number): number {
+    const h = this.#canvasWrapper.height;
+    return (h + y) % h;
+  }
+}
