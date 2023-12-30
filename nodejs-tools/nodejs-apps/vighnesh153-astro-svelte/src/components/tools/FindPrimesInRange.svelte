96d4850c9c22862fc9e83e172be4679162b29e6d
vighnesh153 <vighnesh.raut13@gmail.com>
2023-12-30 11:09:31 +0530

    feat: add some prime number calculators

---
+++ nodejs-tools/nodejs-apps/vighnesh153-astro-svelte/src/components/tools/FindPrimesInRange.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import Button from '@/components/Button.svelte';
+  import { Queue, isPrime } from '@vighnesh153/utils';
+
+  let minN: number = 0;
+  let maxN: number = 0;
+  let primesInRange: number[] = [];
+
+  function onFind() {
+    const start = Math.ceil(minN);
+    const end = Math.floor(maxN);
+
+    const primes = new Queue<number>();
+    for (let i = start; i <= end; i++) {
+      if (isPrime(i)) {
+        primes.pushRight(i);
+      }
+    }
+    primesInRange = primes.toArray();
+  }
+</script>
+
+<p class=" text-text">Find primes in between a range</p>
+
+<div class="mt-4 flex gap-6 items-center">
+  <input class="p-2 text-secondary" type="number" bind:value={minN} />
+  <input class="p-2 text-secondary" type="number" bind:value={maxN} />
+
+  <Button variant="primary" on:click={onFind}>Find</Button>
+</div>
+
+<div>
+  {#if primesInRange.length > 0}
+    <p class="text-text2">Count: <span class="text-accent">{primesInRange.length}</span></p>
+    <p class="text-text2">Values: <span class="text-text">{primesInRange.join(', ')}</span></p>
+  {/if}
+</div>
---
+++ nodejs-tools/nodejs-apps/vighnesh153-astro-svelte/src/components/tools/IsPrime.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  import Button from '@/components/Button.svelte';
+  import { isPrime, primeFactorize, type PrimeFactorsCount } from '@vighnesh153/utils';
+
+  let n: number = 0;
+
+  let nIsPrime: boolean | null = null;
+  let result = '';
+  let primeFactors: PrimeFactorsCount = {};
+
+  function onClick() {
+    nIsPrime = isPrime(n);
+    if (nIsPrime) {
+      result = `${n} is a prime number`;
+    } else {
+      result = `${n} is not a prime number`;
+    }
+    primeFactors = primeFactorize(n);
+  }
+</script>
+
+<p class=" text-text">Check if a number is a prime number (max 16 digit numbers)</p>
+
+<div class="mt-4 flex gap-6 items-center">
+  <input
+    class="p-2 text-secondary"
+    type="number"
+    value={n}
+    on:input={(e) => {
+      n = parseInt(
+        `${e.currentTarget.value}`
+          .split('')
+          .filter((ch) => '0123456789'.includes(ch))
+          .join(''),
+        10
+      );
+      if (isNaN(n)) {
+        n = 1;
+      }
+    }}
+    min="0"
+    step="1"
+  />
+
+  <Button variant="primary" on:click={onClick}>Check</Button>
+</div>
+
+<div>
+  {#if nIsPrime !== null}
+    <p class="text-text2">{result}</p>
+  {/if}
+
+  {#if Object.keys(primeFactors).length > 0}
+    <p>
+      <span class="text-text2">Prime Factorization: </span>
+      {#each Object.entries(primeFactors) as [prime, factors], index}
+        <span class="text-accent">
+          {prime}
+          {#if factors > 1}
+            <sup>{factors}</sup>
+          {/if}
+          {index === Object.keys(primeFactors).length - 1 ? '' : ' x '}
+        </span>
+      {/each}
+    </p>
+  {/if}
+</div>
