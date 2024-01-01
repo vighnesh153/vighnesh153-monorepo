cb7c43b0eb3745486624b0b0cc3b6aa03346dcf7
vighnesh153 <vighnesh.raut13@gmail.com>
2024-01-01 22:44:09 +0530

    feat: remove dependency on godotenv and move secrets file closer to app

---
+++ golang-tools/apps/identity/identity-app/.env-template.sh
@@ -0,0 +1,6 @@
+export GOOGLE_CLIENT_ID="your google oauth client id"
+export GOOGLE_CLIENT_SECRET="your google oauth client secret"
+
+export AUTH_SERVER_ROOT_URI="http://localhost:4321"
+export UI_ROOT_HOST="staging.vighnesh153.dev"
+export AUTH_UI_CALLBACK_URI="https://staging.vighnesh153.dev/auth/callback"
