c1d93a4be41518a51e7a556c409641a768416628
vighnesh153 <vighnesh.raut13@gmail.com>
2024-01-15 19:57:53 +0530

    feat: add repl for monkey-lang

---
+++ golang-tools/interpreters/monkey-lang/repl/repl.go
@@ -0,0 +1,30 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"vighnesh153/interpreters/monkey-lang/lexer"
+	"vighnesh153/interpreters/monkey-lang/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprint(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.NewLexer(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
