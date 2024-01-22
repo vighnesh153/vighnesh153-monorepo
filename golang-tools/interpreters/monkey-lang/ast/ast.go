9fddf3cb7bfbd4ad31e900fdbca112e881d52e5c
vighnesh153 <vighnesh.raut13@gmail.com>
2024-01-22 17:25:53 +0530

    feat: add infix-expression parsing

--- golang-tools/interpreters/monkey-lang/ast/ast.go
+++ golang-tools/interpreters/monkey-lang/ast/ast.go
@@ -120,7 +120,7 @@ func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
 
 type PrefixExpression struct {
-	Token    token.Token // the prefix token, e.g. !
+	Token    token.Token // the prefix token, e.g., !
 	Operator string
 	Right    Expression
 }
@@ -132,6 +132,25 @@ func (pe *PrefixExpression) String() string {
 	out.WriteString("(")
 	out.WriteString(pe.Operator)
 	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+	return out.String()
+}
+
+type InfixExpression struct {
+	Token    token.Token // the infix operator token, e.g., +
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (ie *InfixExpression) expressionNode()      {}
+func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
 	out.WriteString(")")
 	return out.String()
 }
