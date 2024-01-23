4c348a76584dfffe3d94290c53c4365bb6ca9796
vighnesh153 <vighnesh.raut13@gmail.com>
2024-01-23 13:38:24 +0530

    feat: add function-call-expression parsing

--- golang-tools/interpreters/monkey-lang/ast/ast.go
+++ golang-tools/interpreters/monkey-lang/ast/ast.go
@@ -223,3 +223,24 @@ func (fl *FunctionLiteral) String() string {
 	out.WriteString(fl.Body.String())
 	return out.String()
 }
+
+type CallExpression struct {
+	Token     token.Token // The '(' token
+	Function  Expression  // Identifier or FunctionLiteral or some expression evaluating to a function
+	Arguments []Expression
+}
+
+func (ce *CallExpression) expressionNode()      {}
+func (ce *CallExpression) TokenLiteral() string { return ce.Token.Literal }
+func (ce *CallExpression) String() string {
+	var out bytes.Buffer
+	args := []string{}
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+	return out.String()
+}
