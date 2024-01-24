92f36568b161f928fd867b9b3bdec84bd19a4bd0
vighnesh153 <vighnesh.raut13@gmail.com>
2024-01-24 18:35:25 +0530

    feat: add evaluation of integer literals

---
+++ golang-tools/interpreters/monkey-lang/evaluator/evaluator_test.go
@@ -0,0 +1,43 @@
+package evaluator
+
+import (
+	"testing"
+	"vighnesh153/interpreters/monkey-lang/lexer"
+	"vighnesh153/interpreters/monkey-lang/object"
+	"vighnesh153/interpreters/monkey-lang/parser"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.NewLexer(input)
+	p := parser.NewParser(l)
+	program := p.ParseProgram()
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d",
+			result.Value, expected)
+		return false
+	}
+	return true
+}
