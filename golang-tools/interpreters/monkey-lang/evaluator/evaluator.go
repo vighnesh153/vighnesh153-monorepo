2e34adfa0a29a9e0be1d35cfeb7fbe2c1dfef404
vighnesh153 <vighnesh.raut13@gmail.com>
2024-01-24 18:41:57 +0530

    feat: add evaluation of boolean literals

--- golang-tools/interpreters/monkey-lang/evaluator/evaluator.go
+++ golang-tools/interpreters/monkey-lang/evaluator/evaluator.go
@@ -5,6 +5,14 @@ import (
 	"vighnesh153/interpreters/monkey-lang/object"
 )
 
+// perf improvement because we don't need to create new objects for null, true or false because their values
+// will always be constant as opposed to integers (the values can be any integer)
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// Statements
@@ -15,6 +23,8 @@ func Eval(node ast.Node) object.Object {
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 	return nil
 }
@@ -25,4 +35,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
