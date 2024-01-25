667009b434b1745f8bf16da4b20715677269cb7b
vighnesh153 <vighnesh.raut13@gmail.com>
2024-01-25 21:30:13 +0530

    feat: add len(string) builtin function

--- golang-tools/interpreters/monkey-lang/object/object.go
+++ golang-tools/interpreters/monkey-lang/object/object.go
@@ -16,6 +16,7 @@ const (
 	NULL_OBJ         = "NULL"
 	FUNCTION_OBJ     = "FUNCTION"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	BUILTIN_OBJ      = "BUILTIN"
 	ERROR_OBJ        = "ERROR"
 )
 
@@ -85,3 +86,11 @@ type String struct {
 
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string  { return s.Value }
+
+type BuiltinFunction func(args ...Object) Object
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
