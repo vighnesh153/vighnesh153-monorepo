c737f309380153cc33e04c924234cc80ba4a03e8
vighnesh153 <vighnesh.raut13@gmail.com>
2024-01-25 18:12:45 +0530

    feat: add call-expression evaluation and environment-closure

--- golang-tools/interpreters/monkey-lang/object/environment.go
+++ golang-tools/interpreters/monkey-lang/object/environment.go
@@ -2,12 +2,17 @@ package object
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
+
 func (e *Environment) Set(name string, val Object) Object {
 	e.store[name] = val
 	return val
@@ -15,5 +20,11 @@ func (e *Environment) Set(name string, val Object) Object {
 
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{store: s}
+	return &Environment{store: s, outer: nil}
+}
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
 }
