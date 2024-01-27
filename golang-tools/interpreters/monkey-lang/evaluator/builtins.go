e9ed1c814676747f998974689c5692d5f55519d1
vighnesh153 <vighnesh.raut13@gmail.com>
2024-01-27 22:25:22 +0530

    feat: add puts builtin

--- golang-tools/interpreters/monkey-lang/evaluator/builtins.go
+++ golang-tools/interpreters/monkey-lang/evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "vighnesh153/interpreters/monkey-lang/object"
+import (
+	"fmt"
+	"vighnesh153/interpreters/monkey-lang/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -108,6 +111,15 @@ var builtins = map[string]*object.Builtin{
 			copy(newElements, arr.Elements)
 			newElements[length] = args[1]
 			return &object.Array{Elements: newElements}
+		},
+	},
+
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 }
