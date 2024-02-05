1ca9da2353d1ea844810e0e90c378502cc566be3
vighnesh153 <vighnesh.raut13@gmail.com>
2024-02-05 23:24:55 +0530

    build: fix build issues

--- jvm-tools/interpreters/languages/java-lang-common/build.gradle.kts
+++ jvm-tools/interpreters/languages/java-lang-common/build.gradle.kts
@@ -27,11 +27,3 @@ testing {
         }
     }
 }
-
-// Apply a specific Java toolchain to ease working on different environments.
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-    }
-}
-
