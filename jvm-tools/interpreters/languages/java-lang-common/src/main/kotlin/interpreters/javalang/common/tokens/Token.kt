30d0bcebd175aa664747fac53078a932236baab5
vighnesh153 <vighnesh.raut13@gmail.com>
2024-02-07 14:45:14 +0530

    feat: add symbols and identifier lexer

--- jvm-tools/interpreters/languages/java-lang-common/src/main/kotlin/interpreters/javalang/common/tokens/Token.kt
+++ jvm-tools/interpreters/languages/java-lang-common/src/main/kotlin/interpreters/javalang/common/tokens/Token.kt
@@ -3,4 +3,8 @@ package interpreters.javalang.common.tokens
 data class Token(
     val tokenType: TokenType,
     val tokenLiteral: String
-)
+) {
+    companion object {
+        val EOF = Token(tokenType = TokenType.EOF, tokenLiteral = "")
+    }
+}
