2987fa69ff9229b8870f4d54a4d78047b2400e78
vighnesh153 <vighnesh.raut13@gmail.com>
2024-02-07 18:59:23 +0530

    feat: add double-equals lexing and fix bug in identifier lexing

--- jvm-tools/interpreters/languages/java-lang-common/src/test/kotlin/interpreters/javalang/common/lexer/LexerTest.kt
+++ jvm-tools/interpreters/languages/java-lang-common/src/test/kotlin/interpreters/javalang/common/lexer/LexerTest.kt
@@ -9,12 +9,22 @@ class LexerTest {
     @Test
     fun lexerNextToken() {
         val input = """
-,;@+-*/\%!&|^?:.~'"`
+
+,;@+-*/\%!&|^?:.~
+
+'"`
+
 (){}[]<>
+
 abc _aa a123 __11 _
+
+a == b== c
+
         """.trimIndent()
 
         val expectedTokens = listOf(
+
+            // ,;@+-*/\%!&|^?:.~
             ExpectedToken(id = 0, tokenType = TokenType.COMMA, tokenLiteral = TokenType.COMMA.value),
             ExpectedToken(id = 1, tokenType = TokenType.SEMICOLON, tokenLiteral = TokenType.SEMICOLON.value),
             ExpectedToken(id = 2, tokenType = TokenType.AT_SIGN, tokenLiteral = TokenType.AT_SIGN.value),
@@ -32,9 +42,13 @@ abc _aa a123 __11 _
             ExpectedToken(id = 14, tokenType = TokenType.COLON, tokenLiteral = TokenType.COLON.value),
             ExpectedToken(id = 15, tokenType = TokenType.DOT, tokenLiteral = TokenType.DOT.value),
             ExpectedToken(id = 16, tokenType = TokenType.TILDE, tokenLiteral = TokenType.TILDE.value),
+
+            // '"`
             ExpectedToken(id = 17, tokenType = TokenType.SINGLE_QUOTE, tokenLiteral = TokenType.SINGLE_QUOTE.value),
             ExpectedToken(id = 18, tokenType = TokenType.DOUBLE_QUOTE, tokenLiteral = TokenType.DOUBLE_QUOTE.value),
             ExpectedToken(id = 19, tokenType = TokenType.BACKTICK, tokenLiteral = TokenType.BACKTICK.value),
+
+            // (){}[]<>
             ExpectedToken(
                 id = 20,
                 tokenType = TokenType.LEFT_PARENTHESIS,
@@ -75,12 +89,23 @@ abc _aa a123 __11 _
                 tokenType = TokenType.RIGHT_ANGLE_BRACKET,
                 tokenLiteral = TokenType.RIGHT_ANGLE_BRACKET.value
             ),
+
+            // abc _aa a123 __11 _
             ExpectedToken(id = 28, tokenType = TokenType.IDENTIFIER, tokenLiteral = "abc"),
             ExpectedToken(id = 29, tokenType = TokenType.IDENTIFIER, tokenLiteral = "_aa"),
             ExpectedToken(id = 30, tokenType = TokenType.IDENTIFIER, tokenLiteral = "a123"),
             ExpectedToken(id = 31, tokenType = TokenType.IDENTIFIER, tokenLiteral = "__11"),
             ExpectedToken(id = 32, tokenType = TokenType.IDENTIFIER, tokenLiteral = "_"),
-            ExpectedToken(id = 33, tokenType = Token.EOF.tokenType, tokenLiteral = Token.EOF.tokenLiteral),
+
+            // a == b== c
+            ExpectedToken(id = 33, tokenType = TokenType.IDENTIFIER, tokenLiteral = "a"),
+            ExpectedToken(id = 34, tokenType = TokenType.DOUBLE_EQUALS, tokenLiteral = "=="),
+            ExpectedToken(id = 35, tokenType = TokenType.IDENTIFIER, tokenLiteral = "b"),
+            ExpectedToken(id = 36, tokenType = TokenType.DOUBLE_EQUALS, tokenLiteral = "=="),
+            ExpectedToken(id = 37, tokenType = TokenType.IDENTIFIER, tokenLiteral = "c"),
+
+            // eof
+            ExpectedToken(id = -1, tokenType = Token.EOF.tokenType, tokenLiteral = Token.EOF.tokenLiteral),
         )
 
         // In the expectedTokens, if ids are not unique, throw error
@@ -93,9 +118,8 @@ abc _aa a123 __11 _
         for (expectedToken in expectedTokens) {
             val actualToken = lexer.nextToken()
 
-            assertEquals("${1}", "1")
-            assertEquals(expectedToken.tokenType.name, actualToken.tokenType.name)
-            assertEquals(expectedToken.tokenLiteral, actualToken.tokenLiteral)
+            assertEquals(expectedToken.tokenType.name, actualToken.tokenType.name, "id: ${expectedToken.id}")
+            assertEquals(expectedToken.tokenLiteral, actualToken.tokenLiteral, "id: ${expectedToken.id}")
         }
     }
 }
