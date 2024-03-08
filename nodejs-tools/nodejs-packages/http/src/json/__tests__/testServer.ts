656fe93265daba22c3a7fa29a28893dfeaf1bfde
vighnesh153 <vighnesh.raut13@gmail.com>
2024-03-08 10:58:55 +0530

    feat: add abort request handling

--- nodejs-tools/nodejs-packages/http/src/json/__tests__/testServer.ts
+++ nodejs-tools/nodejs-packages/http/src/json/__tests__/testServer.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { sleep } from '@vighnesh153/utils';
 
 const app = express();
 
@@ -10,8 +11,13 @@ app.get('/200', (req, res) => {
   });
 });
 
-app.get('/500', (req, res) => {
-  res.status(500).send('Oh crap! Something went wrong...');
+app.get('/201', async (req, res) => {
+  await sleep(1000);
+  res.json({
+    message: '200',
+    receivedSearchParams: req.query,
+    receivedHeaders: req.headers,
+  });
 });
 
 app.get('/401', (req, res) => {
@@ -25,6 +31,10 @@ app.get('/403', (req, res) => {
     receivedHeaders: req.headers,
     cookies: req.cookies,
   });
+});
+
+app.get('/500', (req, res) => {
+  res.status(500).send('Oh crap! Something went wrong...');
 });
 
 export { app };
