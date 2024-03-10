8d955c150171e93b681bad0ecd58661938276247
vighnesh153 <vighnesh.raut13@gmail.com>
2024-03-10 11:03:29 +0530

    test: add json http client impl test

--- nodejs-tools/nodejs-packages/http/src/json/__tests__/JsonHttpClientImpl.test.ts
+++ nodejs-tools/nodejs-packages/http/src/json/__tests__/JsonHttpClientImpl.test.ts
@@ -24,6 +24,11 @@ const receivedSearchParams = {
   pokemon: ['pikachu', 'greninja'],
 };
 
+const postData = {
+  name: 'Pikachu',
+  types: ['thunder', 'normal'],
+};
+
 const getCommonReceivedHeaders = () => ({
   accept: 'application/json',
   'accept-encoding': 'gzip, deflate',
@@ -32,6 +37,11 @@ const getCommonReceivedHeaders = () => ({
   host: serverAddress.slice('http://'.length),
   'sec-fetch-mode': 'cors',
   'user-agent': 'node',
+});
+
+const getPostRequestReceivedHeaders = (contentLength = 47) => ({
+  'content-length': contentLength.toString(),
+  'content-type': 'application/json',
 });
 
 beforeAll(async () => {
@@ -60,6 +70,29 @@ afterAll(async () => {
 test('for GET Request, send headers and query parameters through the request', async () => {
   const client = new JsonHttpClientImpl({ baseUrl: serverAddress });
   const executor = client.get({ path: '/200', headers, queryParameters });
+
+  const response = await executor.execute();
+
+  expect(response.isSuccess()).toBe(true);
+  expect(response.isError()).toBe(false);
+  expect(() => response.getErrorResponse()).toThrowErrorMatchingInlineSnapshot(`[Error: Not an error response]`);
+
+  const successResponse = response.getSuccessResponse();
+  expect(successResponse.type).toBe('success');
+  expect(successResponse.statusCode).toBe(200);
+  expect(successResponse.data).toStrictEqual({
+    message: '200',
+    receivedHeaders: {
+      ...getCommonReceivedHeaders(),
+      ...headers,
+    },
+    receivedSearchParams,
+  });
+});
+
+test('for GET Request, send headers and query parameters through the request using absolute url', async () => {
+  const client = new JsonHttpClientImpl({ baseUrl: serverAddress });
+  const executor = client.get({ path: `${serverAddress}/200`, headers, queryParameters });
 
   const response = await executor.execute();
 
@@ -161,3 +194,135 @@ test('for GET Request, parse the 5xx response as error', async () => {
 });
 
 // todo: post
+
+test('for POST Request, send headers, query parameters and data through the request', async () => {
+  const client = new JsonHttpClientImpl({ baseUrl: serverAddress });
+  const executor = client.post({ path: '/200', headers, queryParameters, data: postData });
+
+  const response = await executor.execute();
+
+  expect(response.isSuccess()).toBe(true);
+  expect(response.isError()).toBe(false);
+  expect(() => response.getErrorResponse()).toThrowErrorMatchingInlineSnapshot(`[Error: Not an error response]`);
+
+  const successResponse = response.getSuccessResponse();
+  expect(successResponse.type).toBe('success');
+  expect(successResponse.statusCode).toBe(200);
+  expect(successResponse.data).toStrictEqual({
+    message: '200',
+    receivedHeaders: {
+      ...getCommonReceivedHeaders(),
+      ...headers,
+      ...getPostRequestReceivedHeaders(),
+    },
+    receivedSearchParams,
+    receivedData: postData,
+  });
+});
+
+test('for POST Request, send headers, query parameters and data through the request using absolute url', async () => {
+  const client = new JsonHttpClientImpl({ baseUrl: serverAddress });
+  const executor = client.post({ path: `${serverAddress}/200`, headers, queryParameters, data: postData });
+
+  const response = await executor.execute();
+
+  expect(response.isSuccess()).toBe(true);
+  expect(response.isError()).toBe(false);
+  expect(() => response.getErrorResponse()).toThrowErrorMatchingInlineSnapshot(`[Error: Not an error response]`);
+
+  const successResponse = response.getSuccessResponse();
+  expect(successResponse.type).toBe('success');
+  expect(successResponse.statusCode).toBe(200);
+  expect(successResponse.data).toStrictEqual({
+    message: '200',
+    receivedHeaders: {
+      ...getCommonReceivedHeaders(),
+      ...headers,
+      ...getPostRequestReceivedHeaders(),
+    },
+    receivedSearchParams,
+    receivedData: postData,
+  });
+});
+
+test('for POST Request, should abort the request', async () => {
+  const client = new JsonHttpClientImpl({ baseUrl: serverAddress });
+  const executor = client.post({ path: '/201', headers, queryParameters, data: postData });
+
+  const [response] = await Promise.all([
+    // actual request (takes 1 second)
+    executor.execute(),
+    // simulate abort after 100 milliseconds
+    new Promise((resolve) => {
+      executor.abortController.abort();
+      resolve(null);
+    }),
+  ]);
+
+  expect(response.isSuccess()).toBe(false);
+  expect(response.isError()).toBe(true);
+  expect(() => response.getSuccessResponse()).toThrowErrorMatchingInlineSnapshot(`[Error: Not a success response]`);
+
+  const errorResponse = response.getErrorResponse();
+  expect(errorResponse.type).toBe('error');
+  expect(errorResponse.statusCode).toBeNull();
+  expect(errorResponse.errorMessage).toBe('This operation was aborted');
+  expect(errorResponse.error).toMatchInlineSnapshot(`[AbortError: This operation was aborted]`);
+});
+
+test('for POST Request, parse the 4xx response as text error', async () => {
+  const client = new JsonHttpClientImpl({ baseUrl: serverAddress });
+  const executor = client.post({ path: '/401', headers, queryParameters, data: postData });
+
+  const response = await executor.execute();
+
+  expect(response.isSuccess()).toBe(false);
+  expect(() => response.getSuccessResponse()).toThrowErrorMatchingInlineSnapshot(`[Error: Not a success response]`);
+  expect(response.isError()).toBe(true);
+
+  const errorResponse = response.getErrorResponse();
+  expect(errorResponse.type).toBe('error');
+  expect(errorResponse.errorMessage).toMatchInlineSnapshot(`"You are not authenticated"`);
+  expect(errorResponse.error).toBeNull();
+});
+
+test('for POST Request, parse the 4xx response as json error', async () => {
+  const client = new JsonHttpClientImpl({ baseUrl: serverAddress });
+  const executor = client.post({ path: '/403', headers, queryParameters, data: postData });
+
+  const response = await executor.execute();
+
+  expect(response.isSuccess()).toBe(false);
+  expect(() => response.getSuccessResponse()).toThrowErrorMatchingInlineSnapshot(`[Error: Not a success response]`);
+  expect(response.isError()).toBe(true);
+
+  const errorResponse = response.getErrorResponse();
+  expect(errorResponse.type).toBe('error');
+  expect(errorResponse.errorMessage).toMatchInlineSnapshot(`"Bad request"`);
+  expect(errorResponse.error).toStrictEqual({
+    message: 'You are not authorized',
+    receivedHeaders: {
+      ...getCommonReceivedHeaders(),
+      ...headers,
+      ...getPostRequestReceivedHeaders(),
+    },
+    receivedSearchParams,
+    receivedData: postData,
+  });
+});
+
+test('for POST Request, parse the 5xx response as error', async () => {
+  const client = new JsonHttpClientImpl({ baseUrl: serverAddress });
+  const executor = client.post({ path: '/500', headers, queryParameters, data: postData });
+
+  const response = await executor.execute();
+
+  expect(response.isSuccess()).toBe(false);
+  expect(() => response.getSuccessResponse()).toThrowErrorMatchingInlineSnapshot(`[Error: Not a success response]`);
+  expect(response.isError()).toBe(true);
+
+  const errorResponse = response.getErrorResponse();
+  expect(errorResponse.type).toBe('error');
+  expect(errorResponse.errorMessage).toMatchInlineSnapshot(`"Something went wrong on the server"`);
+  expect(errorResponse.error).toMatchInlineSnapshot(`"Oh crap! Something went wrong..."`);
+});
