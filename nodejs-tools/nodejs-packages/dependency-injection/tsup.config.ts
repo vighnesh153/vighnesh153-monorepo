28b84c39a09d3b241583c12c27585e644b8e3ac6
vighnesh153 <vighnesh.raut13@gmail.com>
2024-03-10 11:35:39 +0530

    feat: initialize @vighnesh153/dependency-injection

---
+++ nodejs-tools/nodejs-packages/dependency-injection/tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig(() => ({
+  entry: {
+    main: './src/index.ts',
+  },
+  splitting: false,
+  clean: true,
+  minify: true,
+  treeshake: true,
+  format: ['cjs', 'esm'],
+  outExtension({ format }) {
+    let js: string | undefined;
+    if (format === 'cjs') js = `.cjs`;
+    if (format === 'esm') js = `.js`;
+    return { js };
+  },
+}));
