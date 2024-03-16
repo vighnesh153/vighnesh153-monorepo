d68da3cfdb100b61f6f14c2d2df2e328e160ebdd
vighnesh153 <vighnesh.raut13@gmail.com>
2024-03-16 14:23:11 +0530

    test: add constructAuthRedirectUrl missing test

--- nodejs-tools/nodejs-aws-serverless-tools/identity/src/initiateGoogleLogin/constructAuthRedirectUrl.test.ts
+++ nodejs-tools/nodejs-aws-serverless-tools/identity/src/initiateGoogleLogin/constructAuthRedirectUrl.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from 'vitest';
 import { constructAuthRedirectUrl } from './constructAuthRedirectUrl';
-import { FakeLogger } from '../logger';
+import { FakeLogger } from '@vighnesh153/logger';
 
 test('should return null if identityLambdaBaseUri is empty', () => {
   expect(
@@ -33,4 +33,29 @@ test('should return null if both identityLambdaBaseUri and googleClientId is emp
   expect(
     constructAuthRedirectUrl({ identityLambdaBaseUri: '', googleClientId: '', logger: new FakeLogger() })
   ).toBeNull();
+});
+
+test('should construct auth redirect url if both identityLambdaBaseUri and googleClientId are provided', () => {
+  const result = constructAuthRedirectUrl({
+    logger: new FakeLogger(),
+    googleClientId: 'pikachu',
+    identityLambdaBaseUri: 'https://auth-prod.vighnesh153.dev',
+  });
+
+  const actualUrl = new URL(result!);
+
+  const expectedUrl = new URL('https://accounts.google.com/o/oauth2/v2/auth');
+  const queryParams = new URLSearchParams();
+  queryParams.append('redirect_uri', 'https://auth-prod.vighnesh153.dev/googleAuthCallback');
+  queryParams.append('client_id', 'pikachu');
+  queryParams.append('access_type', 'offline');
+  queryParams.append('response_type', 'code');
+  queryParams.append('prompt', 'consent');
+  queryParams.append(
+    'scope',
+    'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email'
+  );
+  expectedUrl.search = queryParams.toString();
+
+  expect(actualUrl.toString()).toBe(expectedUrl.toString());
 });
