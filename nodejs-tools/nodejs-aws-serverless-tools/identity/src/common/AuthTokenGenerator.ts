81c61d27dd3e272fd9ab7c756f344234a03a0850
vighnesh153 <vighnesh.raut13@gmail.com>
2024-04-15 13:19:36 -0700

    feat: change AuthTokenGenerator api

--- nodejs-tools/nodejs-aws-serverless-tools/identity/src/common/AuthTokenGenerator.ts
+++ nodejs-tools/nodejs-aws-serverless-tools/identity/src/common/AuthTokenGenerator.ts
@@ -1,9 +1,7 @@
 import crypto from 'node:crypto';
 
-import { CompleteUserInfo } from '@vighnesh153/types';
-
 export interface AuthTokenGeneratorParams {
-  userInfo: CompleteUserInfo;
+  userId: string;
   cookieSecret: string;
 }
 
@@ -12,8 +10,8 @@ export interface AuthTokenGenerator {
 }
 
 export class AuthTokenGeneratorImpl implements AuthTokenGenerator {
-  generate(params: AuthTokenGeneratorParams): string {
-    const data = `${params.userInfo.userId}-${params.cookieSecret}`;
+  generate({ userId, cookieSecret }: AuthTokenGeneratorParams): string {
+    const data = `${userId}-${cookieSecret}`;
     return crypto.createHash('sha256').update(data, 'binary').digest('hex');
   }
 }
