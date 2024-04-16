b72a73c7599270ad590db7b6112f0d5c018e51a3
vighnesh153 <vighnesh.raut13@gmail.com>
2024-04-16 16:34:06 -0700

    build: update package versions

--- nodejs-tools/nodejs-apps/vighnesh153-astro/astro.config.ts
+++ nodejs-tools/nodejs-apps/vighnesh153-astro/astro.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
-import prefetch from '@astrojs/prefetch';
-import compress from 'astro-compress';
+import compress from '@playform/compress';
 
 const isDevCommandRunning = process.argv[2] === 'dev';
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [prefetch(), compress({ Logger: 2 }), svelte()],
+  integrations: [compress({ Logger: 2 }), svelte()],
+  prefetch: true,
   build: {
     inlineStylesheets: 'auto',
     format: 'file',
