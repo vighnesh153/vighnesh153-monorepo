cdfb13c77810c60d9bbf07026dabebd219524d0c
vighnesh153 <vighnesh.raut13@gmail.com>
2024-04-17 14:50:01 -0700

    feat: add cookie-store impl

---
+++ nodejs-tools/nodejs-packages/cookie-store/src/index.ts
@@ -0,0 +1,33 @@
+import { CookiesStatic } from 'js-cookie';
+
+class JsCookie {
+  static instance: CookiesStatic<string> | null = null;
+  static async getInstance(): Promise<CookiesStatic<string>> {
+    if (this.instance == null) {
+      this.instance = await import('js-cookie').then((module) => module.default);
+    }
+    return this.instance!;
+  }
+}
+
+export interface CookieStoreWrapper {
+  getCookieValue: (name: string) => Promise<string>;
+}
+
+export class CookieStoreWrapperImpl implements CookieStoreWrapper {
+  async getCookieValue(cookieName: string): Promise<string> {
+    if (window.cookieStore?.get) {
+      return window.cookieStore.get(cookieName).then((cookie) => cookie.value);
+    }
+    const jsCookie = await JsCookie.getInstance();
+    return jsCookie.get(cookieName) ?? '';
+  }
+}
+
+declare global {
+  interface Window {
+    cookieStore: {
+      get: (name: string) => Promise<{ value: string }>;
+    };
+  }
+}
