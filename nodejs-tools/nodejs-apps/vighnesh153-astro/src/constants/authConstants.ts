316d143cb05fc4a50ad4e6ecea792e081e6a85ff
vighnesh153 <vighnesh.raut13@gmail.com>
2024-05-07 10:11:59 +0530

    build: update config

--- nodejs-tools/nodejs-apps/vighnesh153-astro/src/constants/authConstants.ts
+++ nodejs-tools/nodejs-apps/vighnesh153-astro/src/constants/authConstants.ts
@@ -1,10 +1,10 @@
 export const authServerOrigins = {
-  default: 'https://dev.auth.vighnesh153.dev',
-  production: 'https://prod.auth.vighnesh153.dev',
+  default: 'https://dev.identity.vighnesh153.dev',
+  production: 'https://prod.identity.vighnesh153.dev',
 };
 
 export const authLoginPaths = {
-  googleLogin: '/auth/google/login',
+  googleLogin: '/initiateGoogleLogin',
 };
 
 export const onSuccessRedirectPathKey = 'on_success_redirect_path';
