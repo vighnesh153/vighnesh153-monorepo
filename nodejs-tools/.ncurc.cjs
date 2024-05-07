c8e4acd81ef94d83e73877a54cb2150277b3de85
vighnesh153 <vighnesh.raut13@gmail.com>
2024-05-07 14:39:42 +0530

    build: pin esbuild-wasm at a working version

--- nodejs-tools/.ncurc.cjs
+++ nodejs-tools/.ncurc.cjs
@@ -2,11 +2,6 @@
  * @type {import('npm-check-updates/build/src/types/RunOptions.d.ts').RunOptions}
  */
 module.exports = {
-  'upgrade': true,
-  'reject': [
-    'mongodb',
-    'mongoose',
-    'next-auth',
-    'mongodb-memory-server'
-  ],
+  upgrade: true,
+  reject: ['esbuild-wasm', 'mongodb', 'mongoose', 'next-auth', 'mongodb-memory-server'],
 };
