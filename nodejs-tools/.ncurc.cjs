fb7b2be560ef801f68c3da00cdd109876fb49a17
vighnesh153 <vighnesh.raut13@gmail.com>
2024-05-18 13:30:38 +0530

    build: update eslint packages

--- nodejs-tools/.ncurc.cjs
+++ nodejs-tools/.ncurc.cjs
@@ -3,5 +3,19 @@
  */
 module.exports = {
   upgrade: true,
-  reject: ['esbuild-wasm', 'mongodb', 'mongoose', 'next-auth', 'mongodb-memory-server'],
+  reject: [
+    'esbuild-wasm',
+    'mongodb',
+    'mongoose',
+    'next-auth',
+    'mongodb-memory-server',
+    '@typescript-eslint/eslint-plugin',
+    '@typescript-eslint/parser',
+    'eslint',
+    'eslint-config-prettier',
+    'eslint-import-resolver-typescript',
+    'eslint-plugin-astro',
+    'eslint-plugin-import',
+    'eslint-plugin-jsx-a11y',
+  ],
 };
