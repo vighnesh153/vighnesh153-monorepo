7138d79a55099486486e2cbc2bcc3732d4b4cd4a
vighnesh153 <vighnesh.raut13@gmail.com>
2024-05-18 09:22:13 +0530

    fix: astro types issue
    https://github.com/PlayForm/Compress/issues/329

--- nodejs-tools/nodejs-apps/vighnesh153-astro/astro.config.ts
+++ nodejs-tools/nodejs-apps/vighnesh153-astro/astro.config.ts
@@ -1,3 +1,5 @@
+import { type AstroIntegration } from 'astro';
+
 import { defineConfig } from 'astro/config';
 
 import compress from '@playform/compress';
@@ -10,7 +12,13 @@ const isDevCommandRunning = process.argv[2] === 'dev';
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [compress({ Logger: 2 }), svelte(), solid({ devtools: true })],
+  integrations: [
+    // Remove the following type gymnastics when the following issue is fixed.
+    // https://github.com/PlayForm/Compress/issues/329
+    compress({ Logger: 2 }) as unknown as AstroIntegration,
+    svelte(),
+    solid({ devtools: true }),
+  ],
   prefetch: true,
   build: {
     inlineStylesheets: 'auto',
