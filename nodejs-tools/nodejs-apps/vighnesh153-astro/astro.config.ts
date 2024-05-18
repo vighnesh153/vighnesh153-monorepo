1a0cd3e90022bcc7e88d00f46f78b02b4d7686b9
vighnesh153 <vighnesh.raut13@gmail.com>
2024-05-18 07:42:56 +0530

    build: integrate solid js in vighnesh153-astro

--- nodejs-tools/nodejs-apps/vighnesh153-astro/astro.config.ts
+++ nodejs-tools/nodejs-apps/vighnesh153-astro/astro.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 
-import svelte from '@astrojs/svelte';
 import compress from '@playform/compress';
+
+import solid from '@astrojs/solid-js';
+import svelte from '@astrojs/svelte';
 
 const isDevCommandRunning = process.argv[2] === 'dev';
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [compress({ Logger: 2 }), svelte()],
+  integrations: [compress({ Logger: 2 }), svelte(), solid({ devtools: true })],
   prefetch: true,
   build: {
     inlineStylesheets: 'auto',
