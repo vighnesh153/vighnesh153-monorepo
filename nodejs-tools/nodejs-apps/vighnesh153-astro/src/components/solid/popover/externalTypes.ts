fae40b50ee5a2d4612c662dff423817b07aca05b
vighnesh153 <vighnesh.raut13@gmail.com>
2024-05-26 19:49:46 +0530

    docs: add component props docs

--- nodejs-tools/nodejs-apps/vighnesh153-astro/src/components/solid/popover/externalTypes.ts
+++ nodejs-tools/nodejs-apps/vighnesh153-astro/src/components/solid/popover/externalTypes.ts
@@ -17,23 +17,56 @@ export type PopoverPlacement =
 export type PopoverLayoutDirection = 'ltr' | 'rtl';
 
 export type PopoverProps = ParentProps<{
+  /**
+   * State to track whether the popover is open or closed.
+   */
   open: boolean;
 
+  /**
+   * Callback to close the popover. Toggling of the open state should be handled by the parent.
+   */
   close: () => void;
 
   // TODO: click outside listener
   // TODO: smart alignment based on scrolling
+  // TODO: add tiny-triangle to popover
+  // TODO: accessibility
 
+  /**
+   * Where to place the floating element relative to its reference element.
+   *
+   * @default 'bottom-center'
+   */
   placement?: PopoverPlacement;
 
-  // Interaction with this element triggers the popover
+  /**
+   * Changes the placement of the floating element to keep it in view.
+   *
+   * @default true
+   */
+  flipPlacement?: boolean;
+
+  /**
+   * Interaction with this element triggers the popover
+   */
   controlElement: JSX.Element;
 
-  // content of the popover
+  /**
+   * Content inside of popover balloon
+   */
   popoverContent: JSX.Element;
 
-  // Scrolling container, which should re-anchor the popover to the control element
+  /**
+   * Scrolling container, which should re-anchor the popover to the control element
+   *
+   * @default window
+   */
   scrollElement?: JSX.Element;
 
+  /**
+   * Whether the layout is a LTR or RTL
+   *
+   * @default 'ltr'
+   */
   layoutDirection?: PopoverLayoutDirection | null;
 }>;
