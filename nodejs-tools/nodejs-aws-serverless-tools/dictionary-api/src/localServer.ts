455482a2d4d74ba2f5c116e0b4b181f9e5ac540e
vighnesh153 <vighnesh.raut13@gmail.com>
2024-06-30 01:03:30 +0530

    chore: cleanup dictionary-api-lambda

--- nodejs-tools/nodejs-aws-serverless-tools/dictionary-api/src/localServer.ts
+++
@@ -1,7 +0,0 @@
-import { app } from './app';
-
-const port = process.env.PORT ?? 3000;
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`⚡️ Server is listening on port: ${port}`);
-});
