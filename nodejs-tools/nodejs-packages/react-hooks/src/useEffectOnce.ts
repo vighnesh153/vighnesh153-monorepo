6b9e2956548adce9850d3a0462bdeb4e2f56032d
vighnesh153 <vighnesh.raut13@gmail.com>
2024-07-08 09:24:05 +0530

    chore: remove react hooks packages

--- nodejs-tools/nodejs-packages/react-hooks/src/useEffectOnce.ts
+++
@@ -1,9 +0,0 @@
-import { EffectCallback, useEffect } from 'react';
-
-/**
- * a modified useEffect hook that only runs once.
- * @param effect
- */
-export function useEffectOnce(effect: EffectCallback) {
-  useEffect(effect, []);
-}
