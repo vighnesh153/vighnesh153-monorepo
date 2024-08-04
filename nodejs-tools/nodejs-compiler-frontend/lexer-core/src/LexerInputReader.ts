e59ddcfae375bc573afd5ea5da74d5da21012976
vighnesh153 <vighnesh.raut13@gmail.com>
2024-08-04 22:01:45 +0530

    feat: add line and column number calculation in lexer-core

--- nodejs-tools/nodejs-compiler-frontend/lexer-core/src/LexerInputReader.ts
+++ nodejs-tools/nodejs-compiler-frontend/lexer-core/src/LexerInputReader.ts
@@ -4,7 +4,11 @@ import { EOF_CHARACTER } from './utils';
 export class LexerInputReader {
   #currentIndex = -1;
   #peekIndex = 0;
+  #previousCharacter: string | EOF_CHARACTER = EOF_CHARACTER;
   #currentCharacter: string | EOF_CHARACTER = EOF_CHARACTER;
+
+  #lineNumber = 1;
+  #columnNumber = 0;
 
   get currentCharacter(): string | EOF_CHARACTER {
     return this.#currentCharacter;
@@ -21,16 +25,30 @@ export class LexerInputReader {
     return this.lexerInput.getCharacterAt(this.#peekIndex);
   }
 
+  get lineNumber(): number {
+    return this.#lineNumber;
+  }
+
+  get columnNumber(): number {
+    return this.#columnNumber;
+  }
+
   constructor(private readonly lexerInput: LexerInput) {
     // Initialize the first character
     this.readNextCharacter();
   }
 
   readNextCharacter(): void {
+    this.#previousCharacter = this.#currentCharacter;
     if (this.#peekIndex >= this.lexerInput.getSize()) {
       this.#currentCharacter = EOF_CHARACTER;
     } else {
       this.#currentCharacter = this.lexerInput.getCharacterAt(this.#peekIndex);
+      this.#columnNumber++;
+    }
+    if (this.#previousCharacter === '\n') {
+      this.#lineNumber++;
+      this.#columnNumber = 1;
     }
     this.#currentIndex = this.#peekIndex;
     this.#peekIndex = Math.min(1 + this.#peekIndex, this.lexerInput.getSize());
