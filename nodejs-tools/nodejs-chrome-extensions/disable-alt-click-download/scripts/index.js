1c50c8489eb936dd3858e55387828f9189117125
vighnesh153 <vighnesh.raut13@gmail.com>
2024-08-09 00:11:14 +0530

    feat: add support for tampermonkey and add google repos

--- nodejs-tools/nodejs-chrome-extensions/disable-alt-click-download/scripts/index.js
+++ nodejs-tools/nodejs-chrome-extensions/disable-alt-click-download/scripts/index.js
@@ -1,15 +1,30 @@
-document.addEventListener('click', (e) => {
-  // not clicked on anchor tag
-  const closestAnchor = e.target?.closest('a');
-  if (!closestAnchor) {
-    return;
-  }
-  // not clicked on alt or option key
-  if (!e.altKey) {
-    return;
-  }
+// ==UserScript==
+// @name         Disable "Alt/Option + Click" shortcut
+// @namespace    https://vighnesh153.dev/
+// @version      2024-08-08
+// @description  Disables "Alt/Option + Click" shortcut combo that downloads the link on Chrome
+// @author       vighnesh153
+// @match        *://*/*
+// @icon         https://raw.githubusercontent.com/vighnesh153/vighnesh153-monorepo/main/nodejs-tools/nodejs-chrome-extensions/disable-alt-click-download/images/logo_48x48.png
+// @grant        none
+// ==/UserScript==
 
-  // disable the "alt/option + click" download shortcut combo
-  e.preventDefault();
-  console.log(`Disabled "alt/option + click" download shortcut combo for link: ${closestAnchor.innerText}`);
-});
+(function () {
+  'use strict';
+
+  document.addEventListener('click', (e) => {
+    // not clicked on anchor tag
+    const closestAnchor = e.target?.closest('a');
+    if (!closestAnchor) {
+      return;
+    }
+    // not clicked on alt or option key
+    if (!e.altKey) {
+      return;
+    }
+
+    // disable the "alt/option + click" download shortcut combo
+    e.preventDefault();
+    console.log(`Disabled "alt/option + click" download shortcut combo for link: ${closestAnchor.innerText}`);
+  });
+})();
