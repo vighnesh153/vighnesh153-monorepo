ba52d32f516d0d35c73114b2b04f43e628d28c50
vighnesh153 <vighnesh.raut13@gmail.com>
2024-08-24 22:25:01 +0530

    build: migrate vighnesh153-astro to sst-v3

--- nodejs-tools/nodejs-apps/vighnesh153-astro/astro.config.ts
+++ nodejs-tools/nodejs-apps/vighnesh153-astro/astro.config.ts
@@ -1,5 +1,3 @@
-import { type AstroIntegration } from 'astro';
-
 import { defineConfig } from 'astro/config';
 
 import compress from '@playform/compress';
@@ -7,24 +5,23 @@ import compress from '@playform/compress';
 import solid from '@astrojs/solid-js';
 import svelte from '@astrojs/svelte';
 
+import aws from 'astro-sst';
+
 const isDevCommandRunning = process.argv[2] === 'dev';
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [
-    // Remove the following type gymnastics when the following issue is fixed.
-    // https://github.com/PlayForm/Compress/issues/329
-    compress({ Logger: 2 }) as unknown as AstroIntegration,
-    svelte(),
-    solid({ devtools: true }),
-  ],
+  integrations: [compress({ Logger: 2 }), svelte(), solid({ devtools: true })],
   prefetch: true,
   build: {
     inlineStylesheets: 'auto',
     format: 'file',
   },
   output: 'static',
+  adapter: aws({
+    deploymentStrategy: 'static',
+  }),
   vite: {
     build: {
       sourcemap: isDevCommandRunning,
--- nodejs-tools/nodejs-apps/vighnesh153-astro/sst.config.ts
+++ nodejs-tools/nodejs-apps/vighnesh153-astro/sst.config.ts
@@ -1,26 +1,77 @@
-import { type SSTConfig } from 'sst';
-import { WebsiteStack } from './sst/WebsiteStack';
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./.sst/platform/config.d.ts" />
 
-const sstConfig: SSTConfig = {
-  config(input) {
-    const { stage } = input;
+const oneYear = '31536000';
+const oneDay = '86400';
+const fiveMinutes = '300';
+
+// Hosted zone: vighnesh153.dev
+const hostedZoneId = 'Z0751561MSQKJ4JAASC2';
+const domainNames = {
+  staging: 'staging.vighnesh153.dev',
+  production: 'vighnesh153.dev',
+};
+
+export default $config({
+  app(input) {
+    const { stage } = input ?? {};
     if (!stage) {
-      throw new Error('Stage is not defined in sstConfig.config() func');
+      throw new Error('Stage is not defined. Please provide stage while running the command');
     }
     return {
       name: `${stage}-Vighnesh153Astro`,
-      region: 'ap-south-2', // hyderabad
+      removal: stage === 'prod' ? 'retain' : 'remove',
+      home: 'aws',
     };
   },
-  stacks(app) {
-    const { stage } = app;
+  async run() {
+    const { stage } = $app;
     if (!stage) {
-      throw new Error('Stage is not defined in sstConfig.stacks() func');
+      throw new Error('Stage is not defined. Please provide stage while running the command');
     }
-    app.stack(WebsiteStack, {
-      stackName: `${stage}-Vighnesh153AstroStack`,
+
+    const domainName = stage === 'prod' ? domainNames.production : domainNames.staging;
+
+    new sst.aws.Astro(`${stage}-Vighnesh153Astro`, {
+      // path: '.',
+      // buildCommand: 'npm run build',
+      // buildOutput: 'dist',
+      // errorPage: '404.html',
+      domain: {
+        name: domainName,
+        dns: sst.aws.dns({
+          zone: hostedZoneId,
+        }),
+        redirects: ['www.' + domainName],
+      },
+      assets: {
+        fileOptions: [
+          // HTML files
+          {
+            files: '**/*.html',
+            // cacheControl: 'max-age=0,no-cache,no-store,must-revalidate',
+            cacheControl: [`max-age=${fiveMinutes}`, `s-max-age=${fiveMinutes}`, 'public', 'must-revalidate'].join(','),
+          },
+          // ico
+          ...['**/*.ico'].map((imageRegex) => ({
+            files: imageRegex,
+            cacheControl: [`max-age=${oneDay}`, `s-max-age=${oneDay}`, 'public', 'must-revalidate'].join(','),
+          })),
+          // images
+          ...['**/*.webp', '**/*.png', '**/*.jpeg', '**/*.jpg'].map((imageRegex) => ({
+            files: imageRegex,
+            cacheControl: [`max-age=${oneYear}`, 'public', 'immutable'].join(','),
+          })),
+          // CSS and JS files
+          ...['*.js', '*.css'].map((jsCssFilesRegex) => ({
+            files: jsCssFilesRegex,
+            cacheControl: [`max-age=${oneYear}`, 'public', 'immutable'].join(','),
+          })),
+        ],
+      },
+      environment: {
+        PUBLIC_VIGHNESH153_STAGE: stage,
+      },
     });
   },
-};
-
-export default sstConfig;
+});
