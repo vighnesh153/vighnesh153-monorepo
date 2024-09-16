aff1c05d053d97218175ceee772b396371507b36
vighnesh153 <vighnesh.raut13@gmail.com>
2024-09-16 23:08:07 +0530

    feat: initialize kotlin lexer

---
+++ nodejs-tools/nodejs-compiler-tools/lexer-kotlin/README.md
@@ -0,0 +1,3 @@
+# @vighnesh153/lexer-kotlin
+
+Converts an kotlin string content in to a list of tokens
---
+++ nodejs-tools/nodejs-compiler-tools/lexer-kotlin/tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig(() => ({
+  entry: {
+    main: './src/index.ts',
+  },
+  splitting: false,
+  clean: true,
+  minify: false,
+  dts: true,
+  treeshake: true,
+  format: ['esm'],
+  outExtension: () => ({ js: `.js` }),
+}));
