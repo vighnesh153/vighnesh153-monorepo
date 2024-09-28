aa1676df23c113853341af6122ef63219868ab6a
vighnesh153 <vighnesh.raut13@gmail.com>
2024-09-28 22:35:39 +0530

    feat: update file upload solidjs component

--- nodejs-tools/nodejs-apps/vighnesh153-astro/src/components/solid/uploader/UploadInputBox.tsx
+++ nodejs-tools/nodejs-apps/vighnesh153-astro/src/components/solid/uploader/UploadInputBox.tsx
@@ -1,3 +1,4 @@
+import { not } from '@vighnesh153/tools-platform-independent';
 import { UploadIcon } from '@/icons/solid';
 import { classes } from '@/utils';
 
@@ -10,30 +11,49 @@ export type UploadInputBoxProps = {
 
 export function UploadInputBox(props: UploadInputBoxProps) {
   const onNewFiles = (files: File[]) => {
-    console.log('new files:', files);
+    console.log('new files selected:', files);
     if (files.length > 0) {
       props.onFilesChange([...files]);
     }
   };
 
   return (
-    <label class="w-full cursor-pointer">
+    <label
+      class={classes(`
+      w-full cursor-pointer 
+    `)}
+      classList={{}}
+    >
       <input type="file" class="hidden" multiple onChange={(e) => onNewFiles(Array.from(e.target?.files ?? []))} />
       <div
-        class={classes(`
+        class={classes(
+          `
           w-full
           px-6 py-20
-          relative
-          
-          border-4 border-dotted border-accent 
-          rounded-2xl
+
+          border-4 border-dotted
+          z-px rounded-2xl
+
+          before:bg-[position:0_0,100%_0,100%_100%,0_100%]
+          before:bg-[size:50%_50%,50%_50%]
+          before:bg-no-repeat
+          before:absolute before:-inset-96 before:z-[-2] 
+
+          after:bg-background after:absolute after:inset-1 after:-z-px
 
           flex 
           flex-col gap-2
           items-center justify-center
-        `)}
+        `
+        )}
         classList={{
-          'animate-pulse': props.draggingOver,
+          'border-[transparent]': props.draggingOver,
+          'border-accent': not(props.draggingOver),
+
+          // eslint-disable-next-line max-len
+          'before:bg-[linear-gradient(transparent,transparent),linear-gradient(transparent,transparent),linear-gradient(transparent,transparent),linear-gradient(theme(colors.accent),theme(colors.background))]':
+            props.draggingOver,
+          'before:animate-spin-slow': props.draggingOver,
         }}
       >
         <div class="bg-[hsl(11,0%,50%,0.3)] rounded-md p-2">
