f23767f8f3df20b3d4699497e3a5fcf22e8a37c2
vighnesh153 <vighnesh.raut13@gmail.com>
2024-10-18 21:58:10 +0530

    refactor: no longer a mono repo

---
+++ deno-tools/api-vighnesh153/.local/tools-platform-independent/http_client_common.ts
@@ -0,0 +1,15 @@
+export interface HttpQueryParameters {
+  [key: string]: string[];
+}
+
+export const HttpHeaderKeys = {
+  contentType: "Content-Type",
+  contentLength: "Content-Length",
+  setCookie: "Set-Cookie",
+} as const;
+
+export const HttpHeaderValues = {
+  contentType: {
+    applicationJson: "application/json",
+  },
+};
