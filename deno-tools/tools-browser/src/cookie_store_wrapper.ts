d12042c470f443e5c2da1258b55c94e6dcc3e8bc
vighnesh153 <vighnesh.raut13@gmail.com>
2024-10-20 00:36:17 +0530

    refactor: move cookie-store to deno browser-tools

--- deno-tools/tools-browser/src/cookie_store_wrapper.ts
+++ deno-tools/tools-browser/src/cookie_store_wrapper.ts
@@ -1,10 +1,12 @@
-import { CookiesStatic } from 'js-cookie';
+import type { CookiesStatic } from "js-cookie";
 
 class JsCookie {
   static instance: CookiesStatic<string> | null = null;
   static async getInstance(): Promise<CookiesStatic<string>> {
     if (this.instance == null) {
-      this.instance = await import('js-cookie').then((module) => module.default);
+      this.instance = await import("js-cookie").then((module) =>
+        module.default
+      );
     }
     return this.instance!;
   }
@@ -17,30 +19,32 @@ export interface CookieStoreWrapper {
 
 export class CookieStoreWrapperImpl implements CookieStoreWrapper {
   async getCookieValue(cookieName: string): Promise<string> {
-    if (window.cookieStore?.get) {
-      return window.cookieStore
+    if (globalThis.cookieStore?.get != null) {
+      return globalThis.cookieStore
         .get(cookieName)
-        .then((cookie) => cookie?.value ?? '')
-        .then((cookieValue) => (cookieValue ? decodeURIComponent(cookieValue) : ''));
+        .then((cookie) => cookie?.value ?? "")
+        .then((
+          cookieValue,
+        ) => (cookieValue ? decodeURIComponent(cookieValue) : ""));
     }
     const jsCookie = await JsCookie.getInstance();
-    return jsCookie.get(cookieName) ?? '';
+    return jsCookie.get(cookieName) ?? "";
   }
 
   async removeCookie(cookieName: string): Promise<void> {
-    if (window.cookieStore?.delete) {
-      return window.cookieStore.delete(cookieName);
+    if (globalThis.cookieStore?.delete != null) {
+      return globalThis.cookieStore.delete(cookieName);
     }
     const jsCookie = await JsCookie.getInstance();
     return jsCookie.remove(cookieName);
   }
 }
 
+// Inspiration: https://stackoverflow.com/a/69429093/8822610
 declare global {
-  interface Window {
-    cookieStore: {
-      get: (name: string) => Promise<{ value: string }>;
-      delete: (name: string) => Promise<void>;
-    };
-  }
+  // deno-lint-ignore no-var
+  var cookieStore: {
+    get: (name: string) => Promise<{ value: string }>;
+    delete: (name: string) => Promise<void>;
+  };
 }
