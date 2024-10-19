a5b2e8476ba3b9fba7eaa32fd401704bf6f21102
vighnesh153 <vighnesh.raut13@gmail.com>
2024-10-20 00:28:31 +0530

    refactor: move fake-data to deno

--- nodejs-tools/nodejs-packages/fake-data/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './randomEmail';
-export * from './randomImage';
-export * from './randomName';
-export * from './randomUuid';
-export * from './randomWithSeed';
--- nodejs-tools/nodejs-packages/fake-data/src/randomName.ts
+++
@@ -1,8 +0,0 @@
-import { randFullName } from '@ngneat/falso';
-
-/**
- * Generates a random full name
- */
-export function randomName(): string {
-  return randFullName();
-}
