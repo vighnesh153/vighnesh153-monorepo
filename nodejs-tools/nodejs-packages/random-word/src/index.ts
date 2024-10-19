fda872426e5750b39511fb7507b89a38004b56ba
vighnesh153 <vighnesh.raut13@gmail.com>
2024-10-19 20:21:52 +0530

    refactor: move random word to deno

--- nodejs-tools/nodejs-packages/random-word/src/index.ts
+++
@@ -1,9 +0,0 @@
-import randomWords from '../data.json';
-
-/**
- * Returns a random word
- */
-export function generateRandomWord() {
-  const randomIndex = Math.floor(Math.random() * randomWords.length);
-  return randomWords[randomIndex];
-}
