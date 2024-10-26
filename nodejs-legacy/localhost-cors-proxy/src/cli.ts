f5a8660faf40e772a2376068136809cf3c35fd93
vighnesh153 <vighnesh.raut13@gmail.com>
2024-10-26 21:17:50 +0530

    refactor: move localhost-cors-proxy to legacy

--- nodejs-legacy/localhost-cors-proxy/src/cli.ts
+++ nodejs-legacy/localhost-cors-proxy/src/cli.ts
@@ -2,7 +2,7 @@
 
 import { Command } from "commander";
 import { startServer } from "./server.ts";
-import packageInfo from "../package.json";
+import packageInfo from "../package.json" with { type: "json" };
 
 const program = new Command();
 
