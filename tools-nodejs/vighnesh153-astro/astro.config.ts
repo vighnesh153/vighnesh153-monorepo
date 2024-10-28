10682702797f335276c7b38bcb68840b0674d0d6
vighnesh153 <vighnesh.raut13@gmail.com>
2024-10-28 21:37:28 +0530

    refactor: vighnesh153-astro

---
+++ tools-nodejs/vighnesh153-astro/astro.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from "astro/config";
+
+import compress from "@playform/compress";
+
+import mdx from "@astrojs/mdx";
+import solid from "@astrojs/solid-js";
+import svelte from "@astrojs/svelte";
+
+import aws from "astro-sst";
+
+const isDevCommandRunning = process.argv[2] === "dev";
+
+// https://astro.build/config
+export default defineConfig({
+  compressHTML: true,
+  integrations: [
+    compress({ Logger: 2 }),
+    svelte(),
+    solid({ devtools: true }),
+    mdx(),
+  ],
+  prefetch: true,
+  build: {
+    inlineStylesheets: "auto",
+    format: "file",
+  },
+  output: "static",
+  adapter: aws({
+    deploymentStrategy: "static",
+  }),
+  vite: {
+    build: {
+      sourcemap: isDevCommandRunning,
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern",
+        },
+      },
+    },
+  },
+});
---
+++ tools-nodejs/vighnesh153-astro/src/components/auth/LoginSuccess.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { onAuthSuccessRedirectPathKey } from "@/utils/index.ts";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    const redirectPath =
+      localStorage.getItem(onAuthSuccessRedirectPathKey) ?? "/";
+    localStorage.removeItem(onAuthSuccessRedirectPathKey);
+    window.location.replace(redirectPath);
+  });
+</script>
+
+<p>Redirecting...</p>
