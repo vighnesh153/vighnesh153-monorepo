efb55512c919c28727753700396e837639510273
vighnesh153 <vighnesh.raut13@gmail.com>
2024-10-29 22:30:14 +0530

    feat: init firebase

--- tools-nodejs/vighnesh153-astro/src/utils/index.ts
+++ tools-nodejs/vighnesh153-astro/src/utils/index.ts
@@ -5,6 +5,7 @@ export * from "./BrowserCookieReader.ts";
 export * from "./classes.ts";
 export * from "./computeInitialsFromName.ts";
 export * from "./factories.ts";
+export * from "./firebase_config.ts";
 export * from "./removeTrailingSlashFromUrlPathName.ts";
 export * from "./stage.ts";
 export * from "./verifyCanvasProjectPath.ts";
