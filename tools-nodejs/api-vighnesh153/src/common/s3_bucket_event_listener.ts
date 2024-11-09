ada19e87620fddc1e26a444752b342fb4216bb80
vighnesh153 <vighnesh.raut13@gmail.com>
2024-11-09 21:18:54 +0530

    feat: update isUploaded flag when a file is uploaded

--- tools-nodejs/api-vighnesh153/src/common/s3_bucket_event_listener.ts
+++ tools-nodejs/api-vighnesh153/src/common/s3_bucket_event_listener.ts
@@ -1,14 +1,39 @@
+import { isStringEmpty, Logger } from "@vighnesh153/tools";
 import { DynamoDBTable } from "@vighnesh153/tools-server/aws_dynamodb";
 import { filesMetadataFields } from "./dynamo_db_table_metadata";
+import { loggerSingletonFactory } from "./factories/logger_factories";
 
 export async function s3BucketEventListener({
   uploadedObjectsKeys,
   filesMetadataDynamoTable,
+
+  // tools
+  logger = loggerSingletonFactory(),
 }: {
   uploadedObjectsKeys: string[];
-  filesMetadataDynamoTable?: DynamoDBTable<
+  filesMetadataDynamoTable: DynamoDBTable<
     { fields: typeof filesMetadataFields; tableName: string }
   >;
+
+  // tools
+  logger: Logger;
 }) {
-  // TODO: implement
+  logger.log(`Received file upload complete event for:`, uploadedObjectsKeys);
+
+  const response = await Promise.allSettled(uploadedObjectsKeys.map((key) => {
+    const fileId = key.split("/").at(-1)?.split(".")?.[0] ?? "";
+
+    if (isStringEmpty(fileId)) {
+      return Promise.reject(
+        new Error(`Failed to extract fileId from "${key}"`),
+      );
+    }
+
+    return filesMetadataDynamoTable.updateOne({
+      key: { fileId },
+      data: { isUploaded: true },
+    });
+  }));
+
+  console.log("Response:", response);
 }
