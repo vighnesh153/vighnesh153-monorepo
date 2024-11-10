f8ac67cc913a95cc3c6fce323ee2f930b970ca4c
vighnesh153 <vighnesh.raut13@gmail.com>
2024-11-10 22:13:49 +0530

    feat: expose isPublic api in metadata fetcher and add some logs

--- tools-deno/tools/src/file_upload/file_upload_metadata_fetcher.ts
+++ tools-deno/tools/src/file_upload/file_upload_metadata_fetcher.ts
@@ -13,5 +13,6 @@ export type FileUploadMetadataFetcherResponse =
 export interface FileUploadMetadataFetcher {
   fetchMetadata(
     files: { clientSideFileId: string; file: File }[],
+    isPublic: boolean,
   ): Promise<FileUploadMetadataFetcherResponse>;
 }
