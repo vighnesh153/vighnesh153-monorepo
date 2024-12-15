07dea9ce34e7ffa80b7167d41f659cd7ec398f68
vighnesh153 <vighnesh.raut13@gmail.com>
2024-12-15 21:19:48 +0530

    feat: configure storage

--- tools-nodejs/vighnesh153-astro/website/src/utils/auth.ts
+++ tools-nodejs/vighnesh153-astro/website/src/utils/auth.ts
@@ -1,15 +1,41 @@
+import { signInWithPopup, signOut } from "firebase/auth";
+
+import { createSnackbar } from "@/store/snackbar.ts";
+
 import {
+  getAuth,
+  getGoogleAuthProvider,
   logAnalyticsEvent,
-  signInToGoogle,
-  signOutFromGoogle,
 } from "./firebase_config.ts";
 
 export async function initiateLoginWithGoogle() {
   logAnalyticsEvent("login_initiate");
-  await signInToGoogle();
+  return signInWithPopup(await getAuth(), getGoogleAuthProvider()).then((res) =>
+    res.user
+  ).catch((e) => {
+    createSnackbar({
+      type: "error",
+      message: `Failed to sign in.`,
+    });
+    console.log(e);
+    console.dir(e);
+    return null;
+  });
 }
 
 export async function initiateLogout() {
   logAnalyticsEvent("logout_initiate");
-  await signOutFromGoogle();
+  await signOut(await getAuth()).then(() =>
+    createSnackbar({
+      type: "success",
+      message: "Successfully signed out!",
+    })
+  ).catch((e) => {
+    createSnackbar({
+      type: "error",
+      message: "Failed to sign out.",
+    });
+    console.log(e);
+    console.dir(e);
+  });
 }
