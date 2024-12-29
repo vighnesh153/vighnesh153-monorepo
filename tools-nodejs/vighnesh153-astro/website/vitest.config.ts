76303e04539166e3db7bc8de2499f45b9e261b32
vighnesh153 <vighnesh.raut13@gmail.com>
2024-12-29 16:36:14 +0530

    fix: broken vitest support for vite 6

--- tools-nodejs/vighnesh153-astro/website/vitest.config.ts
+++ tools-nodejs/vighnesh153-astro/website/vitest.config.ts
@@ -2,12 +2,15 @@
 import { getViteConfig } from "astro/config";
 import solid from "vite-plugin-solid";
 
+import tsconfigPaths from "vite-tsconfig-paths";
+
 export default getViteConfig({
-  plugins: [solid()],
+  plugins: [tsconfigPaths(), solid()],
   resolve: {
     conditions: ["development", "browser"],
   },
   test: {
+    include: ["./src/**/*.test.ts", "./src/**/*.test.tsx"],
     /* for example, use global to avoid globals imports (describe, test, expect): */
     // globals: true,
     exclude: [
